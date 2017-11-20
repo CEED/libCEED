719a6f93da358c30d5ca16e5dcadf4a951c1ab0b
Jed Brown <jed@jedbrown.org>
2017-11-19 18:27:10 -0700

    Feme test: add comparison to reference output

--- tap.sh
+++ tap.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-if ./"$@"; then
+output=$(mktemp $1.XXXX)
+
+if ./"$@" > ${output}; then
     printf "ok $@\n"
 else
     printf "not ok $@\n"
 fi
+if [ -f output/$1.out ]; then
+    if diff -u output/$1.out ${output}; then
+        printf "ok diff $1\n"
+    else
+        printf "not ok diff $1\n"
+    fi
+fi
+rm ${output}
