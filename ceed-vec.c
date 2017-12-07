f140f1ee9bf9b33e30ed5df4ed2f4c420d792714
Tzanio <tzanio@llnl.gov>
2017-12-06 17:15:50 -0800

    Run through code styling

--- ceed-vec.c
+++ ceed-vec.c
@@ -16,64 +16,74 @@
 
 #include <ceed-impl.h>
 
-int CeedVectorCreate(Ceed ceed, CeedInt length, CeedVector *vec) {
-  int ierr;
+int CeedVectorCreate(Ceed ceed, CeedInt length, CeedVector *vec)
+{
+   int ierr;
 
-  if (!ceed->VecCreate) return CeedError(ceed, 1, "Backend does not support VecCreate");
-  ierr = CeedCalloc(1,vec);CeedChk(ierr);
-  (*vec)->ceed = ceed;
-  (*vec)->length = length;
-  ierr = ceed->VecCreate(ceed, length, *vec);CeedChk(ierr);
-  return 0;
+   if (!ceed->VecCreate) { return CeedError(ceed, 1, "Backend does not support VecCreate"); }
+   ierr = CeedCalloc(1,vec); CeedChk(ierr);
+   (*vec)->ceed = ceed;
+   (*vec)->length = length;
+   ierr = ceed->VecCreate(ceed, length, *vec); CeedChk(ierr);
+   return 0;
 }
 
-int CeedVectorSetArray(CeedVector x, CeedMemType mtype, CeedCopyMode cmode, CeedScalar *array) {
-  int ierr;
+int CeedVectorSetArray(CeedVector x, CeedMemType mtype, CeedCopyMode cmode,
+                       CeedScalar *array)
+{
+   int ierr;
 
-  if (!x || !x->SetArray) return CeedError(x ? x->ceed : NULL, 1, "Not supported");
-  ierr = x->SetArray(x, mtype, cmode, array);CeedChk(ierr);
-  return 0;
+   if (!x || !x->SetArray) { return CeedError(x ? x->ceed : NULL, 1, "Not supported"); }
+   ierr = x->SetArray(x, mtype, cmode, array); CeedChk(ierr);
+   return 0;
 }
 
-int CeedVectorGetArray(CeedVector x, CeedMemType mtype, CeedScalar **array) {
-  int ierr;
+int CeedVectorGetArray(CeedVector x, CeedMemType mtype, CeedScalar **array)
+{
+   int ierr;
 
-  if (!x || !x->GetArray) return CeedError(x ? x->ceed : NULL, 1, "Not supported");
-  ierr = x->GetArray(x, mtype, array);CeedChk(ierr);
-  return 0;
+   if (!x || !x->GetArray) { return CeedError(x ? x->ceed : NULL, 1, "Not supported"); }
+   ierr = x->GetArray(x, mtype, array); CeedChk(ierr);
+   return 0;
 }
 
-int CeedVectorGetArrayRead(CeedVector x, CeedMemType mtype, const CeedScalar **array) {
-  int ierr;
+int CeedVectorGetArrayRead(CeedVector x, CeedMemType mtype,
+                           const CeedScalar **array)
+{
+   int ierr;
 
-  if (!x || !x->GetArrayRead) return CeedError(x ? x->ceed : NULL, 1, "Not supported");
-  ierr = x->GetArrayRead(x, mtype, array);CeedChk(ierr);
-  return 0;
+   if (!x || !x->GetArrayRead) { return CeedError(x ? x->ceed : NULL, 1, "Not supported"); }
+   ierr = x->GetArrayRead(x, mtype, array); CeedChk(ierr);
+   return 0;
 }
 
-int CeedVectorRestoreArray(CeedVector x, CeedScalar **array) {
-  int ierr;
+int CeedVectorRestoreArray(CeedVector x, CeedScalar **array)
+{
+   int ierr;
 
-  if (!x || !x->RestoreArray) return CeedError(x ? x->ceed : NULL, 1, "Not supported");
-  ierr = x->RestoreArray(x, array);CeedChk(ierr);
-  return 0;
+   if (!x || !x->RestoreArray) { return CeedError(x ? x->ceed : NULL, 1, "Not supported"); }
+   ierr = x->RestoreArray(x, array); CeedChk(ierr);
+   return 0;
 }
 
-int CeedVectorRestoreArrayRead(CeedVector x, const CeedScalar **array) {
-  int ierr;
+int CeedVectorRestoreArrayRead(CeedVector x, const CeedScalar **array)
+{
+   int ierr;
 
-  if (!x || !x->RestoreArrayRead) return CeedError(x ? x->ceed : NULL, 1, "Not supported");
-  ierr = x->RestoreArrayRead(x, array);CeedChk(ierr);
-  return 0;
+   if (!x || !x->RestoreArrayRead) { return CeedError(x ? x->ceed : NULL, 1, "Not supported"); }
+   ierr = x->RestoreArrayRead(x, array); CeedChk(ierr);
+   return 0;
 }
 
-int CeedVectorDestroy(CeedVector *x) {
-  int ierr;
+int CeedVectorDestroy(CeedVector *x)
+{
+   int ierr;
 
-  if (!*x) return 0;
-  if ((*x)->Destroy) {
-    ierr = (*x)->Destroy(*x);CeedChk(ierr);
-  }
-  ierr = CeedFree(x);CeedChk(ierr);
-  return 0;
+   if (!*x) { return 0; }
+   if ((*x)->Destroy)
+   {
+      ierr = (*x)->Destroy(*x); CeedChk(ierr);
+   }
+   ierr = CeedFree(x); CeedChk(ierr);
+   return 0;
 }
--- t01-vec.c
+++ t01-vec.c
@@ -1,23 +1,25 @@
 #include <ceed.h>
 
-int main(int argc, char **argv) {
-  Ceed ceed;
-  CeedVector x;
-  CeedInt n;
-  CeedScalar a[10];
-  const CeedScalar *b;
+int main(int argc, char **argv)
+{
+   Ceed ceed;
+   CeedVector x;
+   CeedInt n;
+   CeedScalar a[10];
+   const CeedScalar *b;
 
-  CeedInit("/cpu/self", &ceed);
-  n = 10;
-  CeedVectorCreate(ceed, n, &x);
-  for (CeedInt i=0; i<n; i++) a[i] = 10 + i;
-  CeedVectorSetArray(x, CEED_MEM_HOST, CEED_USE_POINTER, a);
-  CeedVectorGetArrayRead(x, CEED_MEM_HOST, &b);
-  for (CeedInt i=0; i<n; i++) {
-    if (10+i != b[i]) CeedError(ceed, (int)i, "Error reading array b[%d] = %f",i,(double)b[i]);
-  }
-  CeedVectorRestoreArrayRead(x, &b);
-  CeedVectorDestroy(&x);
-  CeedDestroy(&ceed);
-  return 0;
+   CeedInit("/cpu/self", &ceed);
+   n = 10;
+   CeedVectorCreate(ceed, n, &x);
+   for (CeedInt i=0; i<n; i++) { a[i] = 10 + i; }
+   CeedVectorSetArray(x, CEED_MEM_HOST, CEED_USE_POINTER, a);
+   CeedVectorGetArrayRead(x, CEED_MEM_HOST, &b);
+   for (CeedInt i=0; i<n; i++)
+   {
+      if (10+i != b[i]) { CeedError(ceed, (int)i, "Error reading array b[%d] = %f",i,(double)b[i]); }
+   }
+   CeedVectorRestoreArrayRead(x, &b);
+   CeedVectorDestroy(&x);
+   CeedDestroy(&ceed);
+   return 0;
 }
