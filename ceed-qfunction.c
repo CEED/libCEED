191b2d13da2838e754e07c8d7074d6148cf3a9b2
Jed Brown <jed@jedbrown.org>
2017-12-12 15:36:53 -0700

    Move comment to CeedQFunctionCreateInterior

--- ceed-qfunction.c
+++ ceed-qfunction.c
@@ -16,6 +16,56 @@
 
 #include <ceed-impl.h>
 
+/**
+  @brief Create a CeedQFunction for evaluating interior (volumetric) terms.
+
+  @param ceed       Ceed library context
+  @param vlength    Vector length.  Caller must ensure that number of quadrature
+                    points is a multiple of vlength.
+  @param nfields    Number of fields/components in input and output arrays
+  @param qdatasize  Size in bytes of quadrature data at each quadrature point.
+  @param inmode     Bitwise OR of evaluation modes for input fields
+  @param outmode    Bitwise OR of evaluation modes for output fields
+  @param f          Function pointer to evaluate action at quadrature points.
+                    See below.
+  @param focca      OCCA identifier "file.c:function_name" for definition of `f`
+  @param qf         constructed QFunction
+  @return 0 on success, otherwise failure
+
+  The arguments of the call-back 'function' are:
+
+   1. [void *ctx][in/out] - user data, this is the 'ctx' pointer stored in
+              the CeedQFunction, set by calling CeedQFunctionSetContext
+
+   2. [void *qdata][in/out] - quadrature points data corresponding to the
+              batch-of-points being processed in this call; the quadrature
+              point index has a stride of 1
+
+   3. [CeedInt nq][in] - number of quadrature points to process
+
+   4. [const CeedScalar *const *u][in] - input fields data at quadrature pts:
+       u[0] - CEED_EVAL_INTERP data: field values at quadrature points in
+              reference space; the quadrature point index has a stride of 1,
+              vector component index has a stride of nq (see argument 3) and
+              values for multiple fields are consequitive in memory (strides
+              will generally vary with the number of components in a field);
+              fields that do not specify CEED_EVAL_INTERP mode, use no memory.
+       u[1] - CEED_EVAL_GRAD data: field gradients at quadrature points in
+              reference space; the quadrature point index has a stride of 1,
+              the derivative direction index has a stride of nq (see argument
+              3), vector component index has a stride of (rdim x nq) where
+              rdim is the dimension of the reference element, and values for
+              multiple fields are consequitive in memory (strides will
+              generally vary with the number of components in a field);
+              fields that do not specify CEED_EVAL_GRAD mode, use no memory.
+       u[2] - CEED_EVAL_DIV data: field divergences ... <same as above>?
+       u[3] - CEED_EVAL_CURL data: field curl ... <same as above>?
+
+   5. [CeedScalar *const *v][out] - output fields data at quadrature points:
+       v[0], v[1], ..., v[3] - use similar layouts as u[] but use the output
+              CeedEvalMode.
+
+*/
 int CeedQFunctionCreateInterior(Ceed ceed, CeedInt vlength, CeedInt nfields,
                                 size_t qdatasize, CeedEvalMode inmode,
                                 CeedEvalMode outmode,
