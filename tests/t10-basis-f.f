3d715cd2859e5b067af94c545d084eda4ba54489
Thilina Rathnayake <thilinarmtb@gmail.com>
2018-01-31 20:33:14 -0600

    Add a test for CeedBasis in Fortran

---
+++ tests/t10-basis-f.f
@@ -0,0 +1,25 @@
+      program test
+
+      include 'ceedf.h'
+
+      integer ceed,err
+      integer b
+
+      character arg*32
+
+      call getarg(1,arg)
+
+      call ceedinit(trim(arg)//char(0),ceed,err)
+      call ceedbasiscreatetensorh1lagrange(ceed,1,1,4,4,
+     $  ceed_gauss_lobatto,b,err)
+c     TODO Not implemented in Fortran yet
+c      call ceedbasisview(b,stdout,err)
+      call ceedbasisdestroy(b,err)
+      call ceedbasiscreatetensorh1lagrange(ceed,1,1,4,4,
+     $  ceed_gauss,b,err)
+c     TODO Not implemented in Fortran yet
+c      call ceedbasisview(b,stdout,err)
+      call ceedbasisdestroy(b,err)
+      call ceeddestroy(ceed,err)
+
+      end
