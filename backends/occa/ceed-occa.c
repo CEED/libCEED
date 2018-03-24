b4337917ce3d9032705735aee3751f71d450a673
camierjs <camier1@llnl.gov>
2018-03-23 18:17:40 -0700

    [occa] cleanup + OCL but tests that use ex1-volume-annulus.okl

--- backends/occa/ceed-occa.c
+++ backends/occa/ceed-occa.c
@@ -58,9 +58,12 @@ static int CeedDestroy_Occa(Ceed ceed) {
 static int CeedInit_Occa(const char *resource, Ceed ceed) {
   int ierr;
   Ceed_Occa *data;
+  const bool cpu = !strcmp(resource, "/cpu/occa");
+  const bool omp = !strcmp(resource, "/omp/occa");
+  const bool ocl = !strcmp(resource, "/ocl/occa");
+  const bool gpu = !strcmp(resource, "/gpu/occa");
   CeedDebug("\033[1m[CeedInit] resource='%s'", resource);
-  if (strcmp(resource, "/cpu/occa") && strcmp(resource, "/omp/occa") &&
-      strcmp(resource, "/ocl/occa") && strcmp(resource, "/gpu/occa"))
+  if (!cpu && !omp && !ocl && !gpu)
     return CeedError(ceed, 1, "OCCA backend cannot use resource: %s", resource);
   ceed->Error = CeedError_Occa;
   ceed->Destroy = CeedDestroy_Occa;
@@ -74,17 +77,19 @@ static int CeedInit_Occa(const char *resource, Ceed ceed) {
 #ifdef CDEBUG
   occaSetVerboseCompilation(true);
 #endif
-
-  const char *mode =
-    (!strcmp(resource, "/gpu/occa")) ? occaGPU :
-    (!strcmp(resource, "/omp/occa")) ? occaOMP :
-    (!strcmp(resource, "/ocl/occa")) ? occaOCL :
-    occaCPU;
+  const char *mode = gpu?occaGPU : omp?occaOMP : ocl ? occaOCL : occaCPU;
   // Now creating OCCA device
   data->device = occaCreateDevice(occaString(mode));
-  if ((resource[1] == 'g' || resource[1] == 'o')
-      && !strcmp(occaDeviceMode(data->device), "Serial"))
-    return CeedError(ceed, 1, "OCCA backend failed to use non-Serial resource");
+  const char *deviceMode = occaDeviceMode(data->device);
+  CeedDebug("\033[1m[CeedInit] deviceMode='%s'", deviceMode);
+  if (cpu && strcmp(occaDeviceMode(data->device), "Serial"))
+    return CeedError(ceed,1, "OCCA backend failed to use Serial resource");
+  if (omp && strcmp(occaDeviceMode(data->device), "OpenMP"))
+    return CeedError(ceed,1, "OCCA backend failed to use OpenMP resource");
+  if (gpu && strcmp(occaDeviceMode(data->device), "CUDA"))
+    return CeedError(ceed,1, "OCCA backend failed to use CUDA resource");
+  if (ocl && strcmp(occaDeviceMode(data->device), "OpenCL"))
+    return CeedError(ceed,1, "OCCA backend failed to use OpenCL resource");
   return 0;
 }
 
