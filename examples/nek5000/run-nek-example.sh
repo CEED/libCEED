d24d3647c2637c9a9909c5b8eb8d05fd3cc87ca2
Thilina Rathnayake <thilinarmtb@gmail.com>
2018-03-28 19:41:43 -0500

    Nek: update run-nek-examples.sh

--- examples/nek5000/run-nek-example.sh
+++ examples/nek5000/run-nek-example.sh
@@ -16,40 +16,89 @@
 
 #!/bin/bash
 
+## Set libCEED directory
+#CEED_DIR=
+
 ###############################################################################
 # DONT'T TOUCH WHAT FOLLOWS !!!
 ###############################################################################
-if [[ $# -ne 4 ]]; then
-  echo "Error: Number of inputs does not equal to 5. Please use the syntax below."
-  echo "./run-nek-example <example_name> <backend> <#mpi_ranks> <rea_name> <rea_and_map_path>"
-  echo "Example ./run-nek-example ex1 4 b3 ./boxes/b3"
-  exit 1
+if [[ "${#BASH_ARGV[@]}" -ne "$#" ]]; then
+   NEK_EXIT_CMD=return
+else
+   NEK_EXIT_CMD=exit
 fi
 
-export LD_LIBRARY_PATH=`cd ../../lib; pwd`:${LD_LIBRARY_PATH}
+: ${CEED_DIR:=`cd ../../; pwd`}
+export LD_LIBRARY_PATH=$CEED_DIR/lib:${LD_LIBRARY_PATH}
 
-ex=$1
-spec=$2
-np=$3
-rea=$4
-reapath=$5
+NEK_THIS_FILE="${BASH_SOURCE[0]}"
+NEK_HELP_MSG="
+$NEK_THIS_FILE [options]
 
-if [[ ! -f $ex ]]; then
-  echo "Example $ex does not exist. Build it with make-nek-examples.sh"
-  exit 1
+options:
+   -h|--help     Print this usage information and exit
+   -e|--example  Example name
+   -c|--ceed     Ceed backend to be used for the run
+   -n|--np       Specify number of MPI ranks for the run
+   -b|--box      Specify the path of the box geometry (./boxes/b3)
+"
+NEK_BOX_DIR=./boxes
+
+nek_ex=ex1
+nek_spec=/cpu/self
+nek_np=4
+nek_box=
+#if [[ $# -ne 4 ]]; then
+#  echo "Error: Number of inputs does not equal to 5. Please use the syntax below."
+#  echo "./run-nek-example <example_name> <backend> <#mpi_ranks> <rea_name> <rea_and_map_path>"
+#  echo "Example ./run-nek-example ex1 4 b3 ./boxes/b3"
+#  exit 1
+#fi
+#export LD_LIBRARY_PATH=`cd ../../lib; pwd`:${LD_LIBRARY_PATH}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -h|--help)
+       echo $NEK_HELP_MSG
+       $NEK_EXIT_CMD
+       ;;
+    -e|--example)
+       shift
+       nek_ex="$1"
+       ;;
+    -c|--ceed)
+       shift
+       nek_spec="$1"
+       ;;
+    -n|--np)
+       shift
+       nek_np="$1"
+       ;;
+    -b|--box)
+       shift
+       nek_box="$1"
+       ;;
+  esac
+  shift
+done
+
+if [[ ! -f ${nek_ex} ]]; then
+  echo "Example ${nek_ex} does not exist. Build it with make-nek-examples.sh"
+  ${NEK_EXIT_CMD} 1
 fi
-if [[ ! -f $reapath/$rea.rea || ! -f $reapath/$rea.map ]]; then
-  echo ".rea/.map file $reapath/$rea does not exist."
-  exit 1
+if [[ ! -f ${NEK_BOX_DIR}/${nek_box}/${nek_box}.rea || \
+	! -f ${NEK_BOX_DIR}/${nek_box}/${nek_box}.map ]]; then
+  echo ".rea/.map file(s) ./boxes/${nek_box}/${nek_box} does not exist."
+  ${NEK_EXIT_CMD} 1
 fi
 
-echo $rea                   >  SESSION.NAME
-echo `cd $reapath; pwd`'/' >>  SESSION.NAME
+echo ${nek_box}                       >  SESSION.NAME
+echo `cd ./boxes/${nek_box}; pwd`'/' >>  SESSION.NAME
 rm -f logfile
 rm -f ioinfo
-mv $ex.log.$np.$rea $ex.log1.$np.$rea 2>/dev/null
+mv ${nek_ex}.log.${nek_np}.${nek_box} ${nek_ex}.log1.${nek_np}.${nek_box} 2>/dev/null
 
-mpiexec -np $np ./$ex $spec > $ex.log.$np.$rea
+mpiexec -np ${nek_np} ./${nek_ex} ${nek_spec} > ${nek_ex}.log.${nek_np}.${nek_box}
 wait $!
 
-echo "Run finished. Output was written to $ex.log.$np.$rea"
+echo "Run finished. Output was written to ${nek_ex}.log.${nek_np}.${nek_box}"
