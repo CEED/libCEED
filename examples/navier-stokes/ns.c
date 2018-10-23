edd460e9a713017020b59fb3aa1a3a48121c73ab
jeremylt <jeremy.thompson@colorado.edu>
2018-10-23 12:48:56 -0600

    Starting adaptive ts

--- examples/navier-stokes/ns.c
+++ examples/navier-stokes/ns.c
@@ -213,6 +213,7 @@ int main(int argc, char **argv) {
   Vec Q, Qloc;
   DM dm;
   TS ts;
+  TSAdapt adapt;
   User user;
 
   ierr = PetscInitialize(&argc, &argv, NULL, help);
@@ -556,8 +557,9 @@ int main(int argc, char **argv) {
   ierr = TSSetFromOptions(ts); CHKERRQ(ierr);
   ierr = TSSetRHSFunction(ts, NULL, RHS_NS, &user); CHKERRQ(ierr);
   ierr = TSSetMaxSteps(ts, 1.e5); CHKERRQ(ierr);
-  ierr = TSSetTimeStep(ts, 1.e-5);CHKERRQ(ierr);
+  ierr = TSSetTimeStep(ts, 1.e-5); CHKERRQ(ierr);
   ierr = TSSetExactFinalTime(ts, TS_EXACTFINALTIME_STEPOVER); CHKERRQ(ierr);
+  ierr = TSGetAdapt(ts, &adapt); CHKERRQ(ierr);
   ierr = TSMonitor_NS(ts, 0, 0., Q, user); CHKERRQ(ierr);
   ierr = TSMonitorSet(ts, TSMonitor_NS, user, NULL); CHKERRQ(ierr);
 
