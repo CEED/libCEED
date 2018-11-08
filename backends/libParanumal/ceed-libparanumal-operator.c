cfaba15e63f90bd0e200f40f66b9e46d201892d8
Yohann Dudouit <yohann.dudouit@gmail.com>
2018-11-07 17:57:26 -0800

    Adds OperatorDestroy and OperatorSetupFields to the libParanumal backend.

--- backends/libParanumal/ceed-libparanumal-operator.c
+++ backends/libParanumal/ceed-libparanumal-operator.c
@@ -17,7 +17,21 @@
 #include <ceed-impl.h>
 #include "ceed-cuda.h"
 
+#define BUFSIZ 16
+
 typedef struct {
+  //libCeed stuff
+  CeedVector  *evecs;   /// E-vectors needed to apply operator (input followed by outputs)
+  CeedScalar **edata;
+  CeedScalar **qdata; /// Inputs followed by outputs
+  CeedScalar **qdata_alloc; /// Allocated quadrature data arrays (to be freed by us)
+  CeedScalar **indata;
+  CeedScalar **outdata;
+  CeedInt    numein;
+  CeedInt    numeout;
+  CeedInt    numqin;
+  CeedInt    numqout;
+  //libParanumal stuff
   mesh_t mesh;
   char[BUFSIZ] fileName;
   char[BUFSIZ] kernelName;
@@ -26,7 +40,31 @@ typedef struct {
 } CeedOperator_libparanumal;
 
 static int CeedOperatorDestroy_libparanumal(CeedOperator op) {
-  //TODO
+  //TODO Destroy the CeedOperator_libparanumal?
+  int ierr;
+  CeedOperator_libparanumal *impl;
+  ierr = CeedOperatorGetData(op, (void*)&impl); CeedChk(ierr);
+
+  for (CeedInt i=0; i<impl->numein+impl->numeout; i++) {
+    ierr = CeedVectorDestroy(&impl->evecs[i]); CeedChk(ierr);
+  }
+  ierr = CeedFree(&impl->evecs); CeedChk(ierr);
+  ierr = CeedFree(&impl->edata); CeedChk(ierr);
+
+  for (CeedInt i=0; i<impl->numqin+impl->numqout; i++) {
+    ierr = CeedFree(&impl->qdata_alloc[i]); CeedChk(ierr);
+  }
+  ierr = CeedFree(&impl->qdata_alloc); CeedChk(ierr);
+  ierr = CeedFree(&impl->qdata); CeedChk(ierr);
+
+  ierr = CeedFree(&impl->indata); CeedChk(ierr);
+  ierr = CeedFree(&impl->outdata); CeedChk(ierr);
+
+  //Free mesh?
+  occaFree(impl->kernelInfo);
+  occaFree(impl->kernel);
+
+  ierr = CeedFree(&op->data); CeedChk(ierr);
   return 1;
 }
 
@@ -39,12 +77,70 @@ static int CeedOperatorSetupFields_libparanumal(CeedOperator op,
                                         CeedInt startq,
                                         const CeedInt numfields,
                                         const CeedInt Q) {
+  int ierr;
+  Ceed ceed;
+  ierr = CeedOperatorGetCeed(op, &ceed); CeedChk(ierr);
+  CeedQFunction qf;
+  ierr = CeedOperatorGetQFunction(op, &qf); CeedChk(ierr);
+  CeedQFunction_libparanumal *qf_data;//FIXME What do we expect?!
+  ierr = CeedQFunctionGetData(qf, (void*)&qf_data); CeedChk(ierr);
+  CeedInt ncomp;
+  CeedInt dim = 1;
+  CeedInt iq=startq;
+  CeedBasis basis;
+  CeedElemRestriction Erestrict;
 
-  //TODO
-  return 1;
+  // Loop over fields
+  for (CeedInt i=0; i<numfields; i++) {
+    CeedEvalMode emode;
+    ierr = CeedQFunctionFieldGetEvalMode(qfields[i], &emode); CeedChk(ierr);
+    if (emode != CEED_EVAL_WEIGHT) {
+      ierr = CeedOperatorFieldGetElemRestriction(ofields[i], &Erestrict); CeedChk(ierr);
+      ierr = CeedElemRestrictionCreateVector(Erestrict, NULL, &evecs[i+starti]); CeedChk(ierr);
+    } else {
+      //TODO something missing?
+    }
+    switch(emode) {
+    case CEED_EVAL_NONE:
+      break; // No action
+    case CEED_EVAL_INTERP:
+      ierr = CeedOperatorFieldGetBasis(ofields[i], &basis); CeedChk(ierr);
+      ierr = CeedQFunctionFieldGetNumComponents(qfields[i], &ncomp); CeedChk(ierr);
+      ierr = CeedBasisGetDimension(basis, &dim); CeedChk(ierr);
+      ierr = CeedMalloc(Q*ncomp, &qdata_alloc[iq]); CeedChk(ierr);
+      qdata[i + starti] = qdata_alloc[iq];
+      iq++;
+      break;
+    case CEED_EVAL_GRAD:
+      ierr = CeedOperatorFieldGetBasis(ofields[i], &basis); CeedChk(ierr);
+      ierr = CeedQFunctionFieldGetNumComponents(qfields[i], &ncomp); CeedChk(ierr);
+      ierr = CeedBasisGetDimension(basis, &dim); CeedChk(ierr);
+      ierr = CeedMalloc(Q*ncomp*dim, &qdata_alloc[iq]); CeedChk(ierr);
+      qdata[i + starti] = qdata_alloc[iq];
+      iq++;
+      break;
+    case CEED_EVAL_WEIGHT: // Only on input fields
+      ierr = CeedOperatorFieldGetBasis(ofields[i], &basis); CeedChk(ierr);
+      ierr = CeedMalloc(Q, &qdata_alloc[iq]); CeedChk(ierr);
+      ierr = CeedBasisApply(basis, 1, CEED_NOTRANSPOSE, CEED_EVAL_WEIGHT,
+                            NULL, qdata_alloc[iq]); CeedChk(ierr);
+      assert(starti==0);
+      ierr = CeedBasisGetDimension(basis, &dim); CeedChk(ierr);
+      qdata[i + starti] = qdata_alloc[iq];
+      indata[i] = qdata[i];
+      break;
+    case CEED_EVAL_DIV: break; // TODO Not implemented
+    case CEED_EVAL_CURL: break; // TODO Not implemented
+    }
+    qf_data->dim = dim;
+  }
+  return 0;
 }
 
 static int CeedOperatorSetup_libparanumal(CeedOperator op) {
+  int ierr;
+  CeedOperator_libparanumal *impl;
+  ierr = CeedOperatorGetData(op, (void*)&impl); CeedChk(ierr);
   //TODO
   return 1;
 }
@@ -55,6 +151,7 @@ static int CeedOperatorApply_libparanumal(CeedOperator op, CeedVector invec,
   CeedOperator_libparanumal *impl;
   ierr = CeedOperatorGetData(op, (void*)&impl); CeedChk(ierr);
 	//Check if the operator Kernel is instanciated, otherwise creates it (jit)
+  // Is it what goes in OperatorSetup?
   if(impl->kernel==NULL){
     impl->mesh;//TODO
     impl->fileName;//TODO
