d97f08af5e56f9892fa608a65ff2428f3ef38674
jeremylt <jeremy.thompson@colorado.edu>
2019-05-14 20:57:00 -0600

    Update memcheck backend to new QFunction interface

--- backends/memcheck/ceed-memcheck-qfunction.c
+++ backends/memcheck/ceed-memcheck-qfunction.c
@@ -25,31 +25,32 @@ static int CeedQFunctionApply_Memcheck(CeedQFunction qf, CeedInt Q,
   void *ctx;
   ierr = CeedQFunctionGetContext(qf, &ctx); CeedChk(ierr);
 
-  int (*f)() = NULL;
-  ierr = CeedQFunctionGetUserFunction(qf, (int (* *)())&f); CeedChk(ierr);
+  CeedQFunctionUser f = NULL;
+  ierr = CeedQFunctionGetUserFunction(qf, &f); CeedChk(ierr);
 
   CeedInt nIn, nOut;
   ierr = CeedQFunctionGetNumArgs(qf, &nIn, &nOut); CeedChk(ierr);
 
+  CeedQFunctionArguments args;
   for (int i = 0; i<nIn; i++) {
-    ierr = CeedVectorGetArrayRead(U[i], CEED_MEM_HOST, &impl->inputs[i]);
+    ierr = CeedVectorGetArrayRead(U[i], CEED_MEM_HOST, &args.in[i]);
     CeedChk(ierr);
   }
   for (int i = 0; i<nOut; i++) {
-    ierr = CeedVectorGetArray(V[i], CEED_MEM_HOST, &impl->outputs[i]);
+    ierr = CeedVectorGetArray(V[i], CEED_MEM_HOST, &args.out[i]);
     CeedChk(ierr);
     CeedInt len;
     ierr = CeedVectorGetLength(V[i], &len); CeedChk(ierr);
     VALGRIND_MAKE_MEM_UNDEFINED(impl->outputs[i], len);
   }
 
-  ierr = f(ctx, Q, impl->inputs, impl->outputs); CeedChk(ierr);
+  ierr = f(ctx, Q, Q, args); CeedChk(ierr);
 
   for (int i = 0; i<nIn; i++) {
-    ierr = CeedVectorRestoreArrayRead(U[i], &impl->inputs[i]); CeedChk(ierr);
+    ierr = CeedVectorRestoreArrayRead(U[i], &args.in[i]); CeedChk(ierr);
   }
   for (int i = 0; i<nOut; i++) {
-    ierr = CeedVectorRestoreArray(V[i], &impl->outputs[i]); CeedChk(ierr);
+    ierr = CeedVectorRestoreArray(V[i], &args.out[i]); CeedChk(ierr);
   }
 
   return 0;
