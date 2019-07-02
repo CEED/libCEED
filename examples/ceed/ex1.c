17f141bb7bd4979460e1276f9ce0024e141d5afc
Yohann Dudouit <yohann.dudouit@gmail.com>
2019-07-02 15:17:10 -0700

    Minor: clean commented code.

--- examples/ceed/ex1.c
+++ examples/ceed/ex1.c
@@ -37,59 +37,6 @@
 
 #include "ex1.qf"
 
-// /// A structure used to pass additional data to f_build_mass
-// struct BuildContext { CeedInt dim, space_dim; };
-
-// /// libCEED Q-function for building quadrature data for a mass operator
-// static int f_build_mass(void *ctx, CeedInt Q,
-//                         const CeedScalar *const *in, CeedScalar *const *out) {
-//   // in[0] is Jacobians with shape [dim, nc=dim, Q]
-//   // in[1] is quadrature weights, size (Q)
-//   struct BuildContext *bc = (struct BuildContext *)ctx;
-//   const CeedScalar *J = in[0], *qw = in[1];
-//   CeedScalar *qd = out[0];
-//   switch (bc->dim + 10*bc->space_dim) {
-//   case 11:
-//     for (CeedInt i=0; i<Q; i++) {
-//       qd[i] = J[i] * qw[i];
-//     }
-//     break;
-//   case 22:
-//     for (CeedInt i=0; i<Q; i++) {
-//       // 0 2
-//       // 1 3
-//       qd[i] = (J[i+Q*0]*J[i+Q*3] - J[i+Q*1]*J[i+Q*2]) * qw[i];
-//     }
-//     break;
-//   case 33:
-//     for (CeedInt i=0; i<Q; i++) {
-//       // 0 3 6
-//       // 1 4 7
-//       // 2 5 8
-//       qd[i] = (J[i+Q*0]*(J[i+Q*4]*J[i+Q*8] - J[i+Q*5]*J[i+Q*7]) -
-//                J[i+Q*1]*(J[i+Q*3]*J[i+Q*8] - J[i+Q*5]*J[i+Q*6]) +
-//                J[i+Q*2]*(J[i+Q*3]*J[i+Q*7] - J[i+Q*4]*J[i+Q*6])) * qw[i];
-//     }
-//     break;
-//   default:
-//     return CeedError(NULL, 1, "dim=%d, space_dim=%d is not supported",
-//                      bc->dim, bc->space_dim);
-//   }
-//   return 0;
-// }
-
-// /// libCEED Q-function for applying a mass operator
-// static int f_apply_mass(void *ctx, CeedInt Q,
-//                         const CeedScalar *const *in, CeedScalar *const *out) {
-//   const CeedScalar *u = in[0], *w = in[1];
-//   CeedScalar *v = out[0];
-//   for (CeedInt i=0; i<Q; i++) {
-//     v[i] = w[i] * u[i];
-//   }
-//   return 0;
-// }
-
-
 // Auxiliary functions.
 int GetCartesianMeshSize(int dim, int order, int prob_size, int nxyz[3]);
 int BuildCartesianRestriction(Ceed ceed, int dim, int nxyz[3], int order,
