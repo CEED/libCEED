d27a002dff3d859590b1a40ea7ca4855e6f4ae77
jeremylt <jeremy.thompson@colorado.edu>
2019-09-01 14:48:31 -0600

    Doc update

--- examples/navier-stokes/navierstokes.c
+++ examples/navier-stokes/navierstokes.c
@@ -1,3 +1,19 @@
+// Copyright (c) 2017, Lawrence Livermore National Security, LLC. Produced at
+// the Lawrence Livermore National Laboratory. LLNL-CODE-734707. All Rights
+// reserved. See files LICENSE and NOTICE for details.
+//
+// This file is part of CEED, a collection of benchmarks, miniapps, software
+// libraries and APIs for efficient high-order finite element and spectral
+// element discretizations for exascale applications. For more information and
+// source code availability see http://github.com/ceed.
+//
+// The CEED research is supported by the Exascale Computing Project 17-SC-20-SC,
+// a collaborative effort of two U.S. Department of Energy organizations (Office
+// of Science and the National Nuclear Security Administration) responsible for
+// the planning and preparation of a capable exascale ecosystem, including
+// software, applications, hardware, advanced system engineering and early
+// testbed platforms, in support of the nation's exascale computing imperative.
+
 //                        libCEED + PETSc Example: Navier-Stokes
 //
 // This example demonstrates a simple usage of libCEED with PETSc to solve a
@@ -13,11 +29,8 @@
 // Sample runs:
 //
 //     navierstokes
-//     navierstokes -ceed /cpu/self
-//     navierstokes -ceed /gpu/occa
-//     navierstokes -ceed /cpu/occa
-//     navierstokes -ceed /omp/occa
-//     navierstokes -ceed /ocl/occa
+//     navierstokes -ceed -problem density_current /cpu/self
+//     navierstokes -ceed -problem advection /gpu/occa
 //
 
 /// @file
