4203e29be3376bc8000c5376d2306d727f491e25
valeriabarra <valeriabarra21@gmail.com>
2019-09-04 13:37:51 -0600

    Homogenize examples by using ncomp and dim

--- examples/mfem/bp3.hpp
+++ examples/mfem/bp3.hpp
@@ -116,7 +116,8 @@ class CeedDiffusionOperator : public mfem::Operator {
     const int ir_order = 2*(order + 2) - 1; // <-----
     const mfem::IntegrationRule &ir =
       mfem::IntRules.Get(mfem::Geometry::SEGMENT, ir_order);
-    CeedInt nqpts, nelem = mesh->GetNE(), dim = mesh->SpaceDimension();
+    CeedInt nqpts, nelem = mesh->GetNE(), dim = mesh->SpaceDimension(),
+            ncomp = dim;
 
     FESpace2Ceed(fes, ir, ceed, &basis, &restr);
 
@@ -144,7 +145,7 @@ class CeedDiffusionOperator : public mfem::Operator {
     // quadrature data) and set its context data.
     CeedQFunctionCreateInterior(ceed, 1, f_build_diff,
                                 f_build_diff_loc, &build_qfunc);
-    CeedQFunctionAddInput(build_qfunc, "dx", dim*dim, CEED_EVAL_GRAD);
+    CeedQFunctionAddInput(build_qfunc, "dx", ncomp*dim, CEED_EVAL_GRAD);
     CeedQFunctionAddInput(build_qfunc, "weights", 1, CEED_EVAL_WEIGHT);
     CeedQFunctionAddOutput(build_qfunc, "rho", dim*(dim+1)/2, CEED_EVAL_NONE);
     CeedQFunctionSetContext(build_qfunc, &build_ctx, sizeof(build_ctx));
