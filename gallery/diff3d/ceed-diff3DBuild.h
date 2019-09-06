719a33001c9b1e2c5d03df60bc1435ec2fe2d84c
jeremylt <jeremy.thompson@colorado.edu>
2019-09-06 10:09:15 -0600

    Gallery - condense diff3DBuild QFunction

--- gallery/diff3d/ceed-diff3DBuild.h
+++ gallery/diff3d/ceed-diff3DBuild.h
@@ -32,34 +32,25 @@ CEED_QFUNCTION(diff3DBuild)(void *ctx, const CeedInt Q,
 
   // Quadrature point loop
   for (CeedInt i=0; i<Q; i++) {
-    // J: 0 3 6   qd: 0 1 2
-    //    1 4 7       1 3 4
-    //    2 5 8       2 4 5
-    const CeedScalar J11 = J[i+Q*0];
-    const CeedScalar J21 = J[i+Q*1];
-    const CeedScalar J31 = J[i+Q*2];
-    const CeedScalar J12 = J[i+Q*3];
-    const CeedScalar J22 = J[i+Q*4];
-    const CeedScalar J32 = J[i+Q*5];
-    const CeedScalar J13 = J[i+Q*6];
-    const CeedScalar J23 = J[i+Q*7];
-    const CeedScalar J33 = J[i+Q*8];
-    const CeedScalar A11 = J22*J33 - J23*J32;
-    const CeedScalar A12 = J13*J32 - J12*J33;
-    const CeedScalar A13 = J12*J23 - J13*J22;
-    const CeedScalar A21 = J23*J31 - J21*J33;
-    const CeedScalar A22 = J11*J33 - J13*J31;
-    const CeedScalar A23 = J13*J21 - J11*J23;
-    const CeedScalar A31 = J21*J32 - J22*J31;
-    const CeedScalar A32 = J12*J31 - J11*J32;
-    const CeedScalar A33 = J11*J22 - J12*J21;
-    const CeedScalar w = qw[i] / (J11*A11 + J21*A12 + J31*A13);
-    qd[i+Q*0] = w * (A11*A11 + A12*A12 + A13*A13);
-    qd[i+Q*1] = w * (A11*A21 + A12*A22 + A13*A23);
-    qd[i+Q*2] = w * (A11*A31 + A12*A32 + A13*A33);
-    qd[i+Q*3] = w * (A21*A21 + A22*A22 + A23*A23);
-    qd[i+Q*4] = w * (A21*A31 + A22*A32 + A23*A33);
-    qd[i+Q*5] = w * (A31*A31 + A32*A32 + A33*A33);
+    // Compute the adjoint
+    CeedScalar A[3][3];
+    for (CeedInt j=0; j<3; j++)
+      for (CeedInt k=0; k<3; k++)
+//      A[k][j] = J[j+1][k+1]*J[j+2][k+2] - J[j+1][k+2]*J[j+2][k+1]
+        A[k][j] = J[i+Q*((j+1)%3+3*((k+1)%3))]*J[i+Q*((j+2)%3+3*((k+2)%3))] -
+                  J[i+Q*((j+1)%3+3*((k+2)%3))]*J[i+Q*((j+2)%3+3*((k+1)%3))];
+
+    // Compute quadrature weight / det(J)
+    const CeedScalar w = qw[i] / (J[i+Q*0]*A[0][0] + J[i+Q*1]*A[1][1] +
+                                  J[i+Q*2]*A[2][2]);
+
+    // Compute geometric factors
+    qd[i+Q*0] = w * (A[0][0]*A[0][0] + A[0][1]*A[0][1] + A[0][2]*A[0][2]);
+    qd[i+Q*1] = w * (A[0][0]*A[1][0] + A[0][1]*A[1][1] + A[0][2]*A[1][2]);
+    qd[i+Q*2] = w * (A[0][0]*A[2][0] + A[0][1]*A[2][1] + A[0][2]*A[2][2]);
+    qd[i+Q*3] = w * (A[1][0]*A[1][0] + A[1][1]*A[1][1] + A[1][2]*A[1][2]);
+    qd[i+Q*4] = w * (A[1][0]*A[2][0] + A[1][1]*A[2][1] + A[1][2]*A[2][2]);
+    qd[i+Q*5] = w * (A[2][0]*A[2][0] + A[2][1]*A[2][1] + A[2][2]*A[2][2]);
   }
 
   return 0;
