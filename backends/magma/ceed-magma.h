6d94819622e2a7ce37e8fd46274ba03caeeb0bdc
nbeams <246972+nbeams@users.noreply.github.com>
2019-11-07 17:11:57 -0500

    fix incorrect merge conflict resolution of header file

--- backends/magma/ceed-magma.h
+++ backends/magma/ceed-magma.h
@@ -56,42 +56,51 @@ typedef struct {
 CEED_INTERN {
 #endif
   void magmablas_dbasis_apply_batched_eval_interp(magma_int_t P, magma_int_t Q,
-      magma_int_t dim, magma_int_t ncomp, const double *dT,
-      CeedTransposeMode tmode, const double *dU, magma_int_t ustride,
-      double *dV, magma_int_t vstride, magma_int_t batchCount);
+                                                  magma_int_t dim, magma_int_t ncomp,
+                                                  const double *dT, CeedTransposeMode tmode,
+                                                  const double *dU, magma_int_t u_elemstride,
+                                                  magma_int_t u_compstride,
+                                                  double *dV, magma_int_t v_elemstride,
+                                                  magma_int_t v_compstride,
+                                                  magma_int_t nelem);
 
   void magmablas_dbasis_apply_batched_eval_grad(magma_int_t P, magma_int_t Q,
-      magma_int_t dim, magma_int_t ncomp, magma_int_t nqpt,
-      const double* dinterp1d, const double *dgrad1d, CeedTransposeMode tmode,
-      const double *dU, magma_int_t ustride, double *dV, magma_int_t vstride,
-      magma_int_t batchCount, magma_int_t dim_ctr);
+                                                magma_int_t dim, magma_int_t ncomp,
+                                                magma_int_t nqpt, const double* dinterp1d,
+                                                const double *dgrad1d, CeedTransposeMode tmode,
+                                                const double *dU, magma_int_t u_elemstride,
+                                                magma_int_t u_compstride, magma_int_t u_dimstride,
+                                                double *dV, magma_int_t v_elemstride,
+                                                magma_int_t v_compstride, magma_int_t v_dimstride,
+                                                magma_int_t dim_id, magma_int_t nelem);
 
-  void magmablas_dbasis_apply_batched_eval_weight(magma_int_t Q,
-      magma_int_t dim, const double *dqweight1d, double *dV,
-      magma_int_t vstride, magma_int_t batchCount);
+  void magmablas_dbasis_apply_batched_eval_weight(magma_int_t Q, magma_int_t dim,
+                                                  const double *dqweight1d, double *dV,
+                                                  magma_int_t v_elemstride,
+                                                  magma_int_t nelem);
 
   void magma_weight(magma_int_t grid, magma_int_t threads, magma_int_t nelem,
                     magma_int_t Q,
                     double *dqweight, double *dv);
 
-  void magma_readDofs(const magma_int_t NCOMP, 
-                      const magma_int_t nnodes, 
-                      const magma_int_t nelem, magma_int_t *indices, 
+  void magma_readDofs(const magma_int_t NCOMP,
+                      const magma_int_t nnodes,
+                      const magma_int_t nelem, magma_int_t *indices,
 	              const double *du, double *dv);
 
-  void magma_readDofsTranspose(const magma_int_t NCOMP, 
-                               const magma_int_t nnodes, 
-                               const magma_int_t nelem, magma_int_t *indices, 
+  void magma_readDofsTranspose(const magma_int_t NCOMP,
+                               const magma_int_t nnodes,
+                               const magma_int_t nelem, magma_int_t *indices,
        	                       const double *du, double *dv);
 
-  void magma_writeDofs(const magma_int_t NCOMP, 
-                       const magma_int_t nnodes, 
-                       const magma_int_t nelem, magma_int_t *indices, 
+  void magma_writeDofs(const magma_int_t NCOMP,
+                       const magma_int_t nnodes,
+                       const magma_int_t nelem, magma_int_t *indices,
 	               const double *du, double *dv);
 
-  void magma_writeDofsTranspose(const magma_int_t NCOMP, 
-                                const magma_int_t nnodes, 
-                                const magma_int_t nelem, magma_int_t *indices, 
+  void magma_writeDofsTranspose(const magma_int_t NCOMP,
+                                const magma_int_t nnodes,
+                                const magma_int_t nelem, magma_int_t *indices,
 	                        const double *du, double *dv);
 
   magma_int_t
