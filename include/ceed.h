2f83bbb9ff72d6ac7d091cac81fc9534c7fc882f
jeremylt <jeremy.thompson@colorado.edu>
2019-11-07 14:46:33 -0700

    Header - remove duplicate declaration

--- include/ceed.h
+++ include/ceed.h
@@ -244,8 +244,6 @@ CEED_EXTERN int CeedElemRestrictionApplyBlock(CeedElemRestriction rstr,
     CeedVector u, CeedVector ru, CeedRequest *request);
 CEED_EXTERN int CeedElemRestrictionGetMultiplicity(CeedElemRestriction rstr,
     CeedVector mult);
-CEED_EXTERN int CeedElemRestrictionCreateVector(CeedElemRestriction rstr,
-    CeedVector *lvec, CeedVector *evec);
 CEED_EXTERN int CeedElemRestrictionView(CeedElemRestriction rstr, FILE *stream);
 CEED_EXTERN int CeedElemRestrictionDestroy(CeedElemRestriction *rstr);
 
