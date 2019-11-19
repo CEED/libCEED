ab387ee89e647f8a6508455f604f1537b91887ed
jeremylt <jeremy.thompson@colorado.edu>
2019-11-19 11:57:09 -0700

    Tests - fix style in t001

--- tests/t001-ceed.c
+++ tests/t001-ceed.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
   CeedGetPreferredMemType(ceed, (CeedMemType *)&type);
   if (type == -1)
     // LCOV_EXCL_START
-    printf("Error getting preferred memory type. %d \n",type);
+    printf("Error getting preferred memory type. %d \n", type);
   // LCOV_EXCL_STOP
 
   CeedDestroy(&ceed);
