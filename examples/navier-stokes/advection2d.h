a850fc96e5108e548c0d9c9ae1e1258a5ecdbc5a
Jed Brown <jed@jedbrown.org>
2019-12-15 09:15:41 -0700

    nsplex advection2d: choose only the smooth initial condition

--- examples/navier-stokes/advection2d.h
+++ examples/navier-stokes/advection2d.h
@@ -51,9 +51,11 @@ static int Exact_Advection2d(CeedInt dim, CeedScalar time, const CeedScalar X[],
   q[4] = 0;
   CeedScalar r = sqrt(pow(x - x0[0], 2) + pow(y - x0[1], 2));
   CeedScalar E = 1 - r/rc;
-  if (q[4] < E) q[4] = E;
-  r = sqrt(pow(x - x1[0], 2) + pow(y - x1[1], 2));
-  if (r <= rc) q[4] = 1;
+  if (0) { // non-smooth initial conditions
+    if (q[4] < E) q[4] = E;
+    r = sqrt(pow(x - x1[0], 2) + pow(y - x1[1], 2));
+    if (r <= rc) q[4] = 1;
+  }
   r = sqrt(pow(x - x2[0], 2) + pow(y - x2[1], 2));
   E = (r <= rc) ? .5 + .5*cos(r*M_PI/rc) : 0;
   if (q[4] < E) q[4] = E;
