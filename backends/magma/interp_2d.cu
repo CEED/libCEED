8e704915c8786cca7d5e46c6a7e86453417c2b3e
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2020-04-07 15:30:44 -0400

    add checks for kernel resources

--- backends/magma/interp_2d.cu
+++ backends/magma/interp_2d.cu
@@ -15,6 +15,7 @@
 // testbed platforms, in support of the nation's exascale computing imperative.
 
 #include <ceed.h>
+#include <cuda.h>    // for CUDA_VERSION
 #include <magma_v2.h>
 #include "magma_tc_device.cuh"
 #include "interp_device.cuh"
@@ -122,14 +123,27 @@ magma_interp_2d_kernel_driver(
                       T *dV, magma_int_t v_elstride, magma_int_t v_compstride, 
                 magma_int_t nelem, magma_queue_t queue)
 {
+    magma_device_t device;
+    magma_getdevice( &device );
+    magma_int_t shmem_max, nthreads_max;
     const int MAXPQ = maxpq(P,Q);
 
     magma_int_t shmem  = 0;
     shmem += P*Q    *sizeof(T);  // for sT
     shmem += P*MAXPQ*sizeof(T);  // for reforming rU we need PxP, and for the intermediate output we need PxQ    
     magma_int_t nthreads = MAXPQ; 
-    
-    if( nthreads > 1024 || shmem >= 48000 ) {
+
+    cudaDeviceGetAttribute (&nthreads_max, cudaDevAttrMaxThreadsPerBlock, device);
+    #if CUDA_VERSION >= 9000
+    cudaDeviceGetAttribute (&shmem_max, cudaDevAttrMaxSharedMemoryPerBlockOptin, device);
+    if(shmem <= shmem_max) {
+        cudaFuncSetAttribute(interp_generic_kernel<P, Q>, cudaFuncAttributeMaxDynamicSharedMemorySize, shmem);
+    }
+    #else
+    cudaDeviceGetAttribute (&shmem_max, cudaDevAttrMaxSharedMemoryPerBlock, device);
+    #endif    // CUDA_VERSION >= 9000
+   
+    if( nthreads > nthreads_max || shmem > shmem_max ) {
         return 1;    // launch failed
     }
     else { 
