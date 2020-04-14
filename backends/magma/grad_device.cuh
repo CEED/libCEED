b82c968612a21088a28247129c83bbb5f972019b
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2020-04-14 15:35:40 -0400

    device routines for grad basis action (2D/3D)

--- backends/magma/grad_device.cuh
+++ backends/magma/grad_device.cuh
@@ -20,8 +20,9 @@
 #define maxpq(p,q)    (p > q ? p : q)
 
 // macros to abstract access of shared memory and reg. file
-#define sT(i,j)          sT[(j) * P + (i)]
-#define sTmp(i,j,ldw)    sTmp[(j)*(ldw) + (i)]
+#define sT(i,j)           sT[(j) * P + (i)]
+#define sTmp(i,j,ldw)     sTmp[(j)*(ldw) + (i)]
+#define sTmp2(i,j,ldw)    sTmp2[(j)*(ldw) + (i)]
 #define rU(idim,icomp,i) rU[(idim)*NCOMP*P + (icomp)*P + (i)]
 #define rV(idim,icomp,i) rV[(idim)*NCOMP*Q + (icomp)*Q + (i)]
 
@@ -51,7 +52,32 @@ make_zero<magmaDoubleComplex>()
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
-// interp basis action (1D)
+// init scalar to one
+template<typename T>
+__device__ __inline__ T
+make_one()
+{
+    return 1;
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+// init scalar to zero -- specialization
+template<>
+__device__ __inline__ magmaFloatComplex
+make_one<magmaFloatComplex>()
+{
+    return MAGMA_C_ONE;
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+// init scalar to zero -- specialization
+template<>
+__device__ __inline__ magmaDoubleComplex
+make_one<magmaDoubleComplex>()
+{
+    return MAGMA_Z_ONE;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// grad basis action (1D)
 template<typename T, int DIM, int NCOMP, int P, int Q>
 static __device__ __inline__ void
 magma_grad_1d_device( 
@@ -72,11 +98,164 @@ magma_grad_1d_device(
         for(int icomp = 0; icomp < NCOMP; icomp++) {
             rv = (transT == MagmaTrans) ? sV[icomp][tx] : make_zero<T>();
             for(int i = 0; i < P; i++) {
-                rv += sU[icomp][i] * sT(i,tx); //sT[tx * P + i];	
+                rv += sU[icomp][i] * sT(i,tx);	
             }
             sV[icomp][tx] = rv;
         }
     }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// grad basis action (2D)
+// This function is called two times at a higher level for 2D
+// DIMU  -- for the size of rU[DIMU * NCOMP * MAXPQ]
+// DIMV  -- for the size of rV[DIMV * NCOMP * MAXPQ]
+// iDIM  -- the index of the outermost loop over dimensions in grad 
+// iDIMU -- which dim index of rU is accessed (always 0 for notrans, 0 or 1 for trans)
+// iDIMV -- which dim index of rV is accessed (0 or 1 for notrans, always 0 for trans)
+// the scalar beta is used to specify whether to accumulate to rV, or overwrite it
+template<typename T, int DIMU, int DIMV, int NCOMP, int P, int Q, int MAXPQ, int iDIM, int iDIMU, int iDIMV>
+static __device__ __inline__ void
+magma_grad_2d_device( 
+    const T *sTinterp, const T *sTgrad, 
+    T rU[DIMU][NCOMP][MAXPQ] , T rV[DIMV][NCOMP][MAXPQ], 
+    T beta, const int tx, T rTmp, T* swork)
+{
+    // Assumptions
+    // 0. This device routine applies grad for one dim only (iDIM), so it should be called twice for 2D
+    // 1. 1D threads of size max(P,Q)
+    // 2. input:  rU[DIMU x NCOMP x P] in registers (per thread)
+    // 3. output: rV[DIMV x NCOMP x Q] in registers (per thread)
+    // 4. Two products per each (dim,component) pair
+    //  4.1 Batch P of (1xP) matrices times (PxQ) matrix => Batch P of (1xQ) matrices
+    //  4.2 Batch 1 of (QxP) matrix   times (PxQ) matrix => (QxQ) matrix
+    // 6. Each thread computes one row of the output of each product
+    // 7. Sync is recommended before and after the call
+
+    for(int icomp = 0; icomp < NCOMP; icomp++){
+        // 1st product -- Batch P of (1xP) matrices [reg] x (PxQ) [shmem] => Batch P of (1xQ) matrices
+        // the batch output P x (1xQ) is written on the fly to shmem
+        if(tx < P) {
+            const int batchid = tx;
+            const int sld     = 1;
+            const T *sT = (iDIM == 0) ? sTgrad : sTinterp;
+            T* sTmp = swork + batchid * (1 * Q);
+            for(int j = 0; j < Q; j++){
+                rTmp = make_zero<T>();
+                for(int i = 0; i < P; i++){
+                    rTmp += rU[iDIMU][icomp][i] * sT(i,j);
+                }
+                sTmp(0,j,sld) = rTmp;
+            }
+        }    // end of: if(tx < P)
+        __syncthreads();
+
+        // 2nd product -- Batch 1 of a (QxP) matrix [shmem] x (PxQ) [shmem] => (QxQ) matrix [reg]
+        if(tx < Q) {
+            const int batchid = 0;
+            const int sld     = Q;
+            const T *sT = (iDIM == 1) ? sTgrad : sTinterp;
+            T* sTmp = swork + batchid * (Q*P);
+            for(int j = 0; j < Q; j++){
+                rTmp = make_zero<T>();
+                for(int i = 0; i < P; i++){
+                    rTmp += sTmp(tx,i,sld) * sT(i,j);
+                }
+                rV[iDIMV][icomp][j] *= beta;
+                rV[iDIMV][icomp][j] += rTmp;
+            }
+        }
+        __syncthreads();
+    }  // loop over NCOMP
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// grad basis action (3D)
+// This function is called three times at a higher level for 3D
+// DIMU  -- for the size of rU[DIMU * NCOMP * MAXPQ]
+// DIMV  -- for the size of rV[DIMV * NCOMP * MAXPQ]
+// iDIM  -- the index of the outermost loop over dimensions in grad 
+// iDIMU -- which dim index of rU is accessed (always 0 for notrans, 0, 1, or 2 for trans)
+// iDIMV -- which dim index of rV is accessed (0, 1, or 2 for notrans, always 0 for trans)
+// the scalar beta is used to specify whether to accumulate to rV, or overwrite it
+template<typename T, int DIMU, int DIMV, int NCOMP, int P, int Q, int MAXPQ, int iDIM, int iDIMU, int iDIMV>
+static __device__ __inline__ void
+magma_grad_3d_device( 
+    const T *sTinterp, const T *sTgrad, 
+    //T rU[DIMU*NCOMP*MAXPQ] , T rV[DIMV*NCOMP*MAXPQ], 
+    T rU[DIMU][NCOMP][MAXPQ] , T rV[DIMV][NCOMP][MAXPQ], 
+    T beta, const int tx, T rTmp, T* swork)
+{
+    // Assumptions
+    // 0. This device routine applies grad for one dim only (iDIM), so it should be thrice for 3D
+    // 1. 1D threads of size max(P,Q)^2
+    // 2. input:  rU[DIMU x NCOMP x P] in registers (per thread)
+    // 3. output: rV[DIMV x NCOMP x Q] in registers (per thread)
+    // 4. Three products per each (dim,component) pair
+    //  4.1 Batch P^2 of (1xP) matrices times (PxQ) matrix => Batch P^2 of (1xQ) matrices
+    //  4.2 Batch P   of (QxP) matrices times (PxQ) matrix => Batch P   of (QxQ) matrices
+    //  4.3 Batch 1   of (Q^2xP) matrix times (PxQ) matrix => (Q^2xQ) matrix
+    // 6. Each thread computes one row of the output of each product
+    // 7. Sync is recommended before and after the call
+
+    T* sW1 = swork;
+    T* sW2 = sW1 + P*P*Q;
+    for(int icomp = 0; icomp < NCOMP; icomp++){
+        // Batch P^2 of (1xP) matrices [reg] times (PxQ) matrix [shmem] => Batch P^2 of (1xQ) matrices [shmem]
+        if(tx < (P*P)) {
+            const int batchid = tx;
+            const int sld     = 1;
+            const T *sT = (iDIM == 0) ? sTgrad : sTinterp;
+            T* sTmp = sW1 + batchid * (1*Q);
+            for(int j = 0; j < Q; j++){
+                rTmp = make_zero<T>();
+                for(int i = 0; i < P; i++){
+                    //rTmp += rU(iDIMU,icomp,i) * sT(i,j);
+                    rTmp += rU[iDIMU][icomp][i] * sT(i,j);
+                }
+                sTmp(0,j,sld) = rTmp;
+            }
+        }    // end of: if(tx < P*P)
+        __syncthreads();
+
+        // Batch P of (QxP) matrices [shmem] times (PxQ) matrix [shmem] => Batch P of (QxQ) matrices [reg]
+        if(tx < (P*Q)) {
+            const int batchid = tx / Q;
+            const int tx_     = tx % Q;
+            const int sld     = Q;
+            const T *sT = (iDIM == 1) ? sTgrad : sTinterp;
+            T* sTmp  = sW1 + batchid * (Q*P); // sTmp is input
+            T* sTmp2 = sW2 + batchid * (Q*Q); // sTmp2 is output
+            for(int j = 0; j < Q; j++){
+                rTmp = make_zero<T>();
+                for(int i = 0; i < P; i++){
+                    rTmp += sTmp(tx_,i,sld) * sT(i,j);
+                }
+                sTmp2(tx_,j,sld) = rTmp;
+            }
+        }
+        __syncthreads();
+
+       // Batch 1 of (Q^2xP) matrices [shmem] times (PxQ) matrix [shmem] => Batch 1 of (Q^2xQ) matrices [reg]
+       if(tx < (Q*Q)) {
+           // No need to declare batchid = (tx  / Q^2) = always zero
+           // No need to declare tx_     = (tx_ % Q^2) = always tx
+           const int sld = Q*Q;
+           const T *sT   = (iDIM == 2) ? sTgrad : sTinterp;
+           T* sTmp = sW2;  // sTmp is input
+           for(int j = 0; j < Q; j++) {
+               rTmp = make_zero<T>();
+               for(int i = 0; i < P; i++) {
+                   rTmp += sTmp(tx,i,sld) * sT(i,j);
+               }
+               //rV(iDIMV,icomp,j) *= beta;
+               //rV(iDIMV,icomp,j) += rTmp;
+               rV[iDIMV][icomp][j] *= beta;
+               rV[iDIMV][icomp][j] += rTmp;
+           }
+       }
+       __syncthreads();
+    }  // loop over NCOMP
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
