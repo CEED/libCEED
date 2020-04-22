046d01bed945cf782f749569aa8996760f02b707
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2020-04-22 11:09:08 -0400

    remove duplicate defs

--- backends/magma/interp_device.cuh
+++ backends/magma/interp_device.cuh
@@ -26,31 +26,6 @@
 #define rV(idim,icomp,i) rV[(idim)*NCOMP*Q + (icomp)*Q + (i)]
 
 //////////////////////////////////////////////////////////////////////////////////////////
-// init scalar to zero
-template<typename T>
-__device__ __inline__ T
-make_zero()
-{
-    return 0;
-}
-//////////////////////////////////////////////////////////////////////////////////////////
-// init scalar to zero -- specialization
-template<>
-__device__ __inline__ magmaFloatComplex
-make_zero<magmaFloatComplex>()
-{
-    return MAGMA_C_ZERO;
-}
-//////////////////////////////////////////////////////////////////////////////////////////
-// init scalar to zero -- specialization
-template<>
-__device__ __inline__ magmaDoubleComplex
-make_zero<magmaDoubleComplex>()
-{
-    return MAGMA_Z_ZERO;
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////
 // interp basis action (1D)
 template<typename T, int DIM, int NCOMP, int P, int Q>
 static __device__ __inline__ void
