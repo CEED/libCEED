d655899a94587eb157858b272448e4c493ef2501
Natalie Beams <246972+nbeams@users.noreply.github.com>
2020-04-22 14:05:55 -0400

    Update names in MAGMA restrictions (#517)
    * update comments in restriction cuda file
    
    * renamed indices --> offsets for restrictions
    
    * fix comments for L-Vector size

--- backends/magma/magma_drestrictApply.cu
+++ backends/magma/magma_drestrictApply.cu
@@ -25,19 +25,17 @@
 // c: component
 // Go from L-vector (du) to E-vector (dv):
 //
-// dv(i, e, c) = du( ind(i, e), c)  
-//         or
-// dv(i, e, c) = du(i, e, c)
+// dv(i, e, c) = du( offsets(i, e) + compstride * c)  
 static __global__ void 
 magma_readDofsOffset_kernel(const int NCOMP, const int compstride,
-                            const int esize, const int nelem, int *indices, 
+                            const int esize, const int nelem, int *offsets, 
                             const double *du, double *dv)
 {
   const int  pid = threadIdx.x;
   const int elem = blockIdx.x;
  
   for (CeedInt i = pid; i < esize; i += blockDim.x) {
-        const CeedInt ind = indices ? indices[i + elem * esize] : i + elem * esize;
+        const CeedInt ind = offsets ? offsets[i + elem * esize] : i + elem * esize;
         for (CeedInt comp = 0; comp < NCOMP; ++comp) {
             dv[i+elem*esize+comp*esize*nelem] = du[ind + compstride * comp];
         }
@@ -76,22 +74,18 @@ magma_readDofsStrided_kernel(const int NCOMP, const int esize, const int nelem,
 // c: component
 // Go from E-vector (du) to L-vector (dv):
 //
-// dv(ind(i, e), c) = du(i, e, c)
-//         or
-// dv(i, e, c) = du(i, e, c)
+// dv(offsets(i, e) + compstride * c) = du(i, e, c)
 static __global__ void 
 magma_writeDofsOffset_kernel(const int NCOMP, const int compstride,
-                             const int esize, const int nelem, int *indices, 
+                             const int esize, const int nelem, int *offsets, 
                              const double *du, double *dv)
 {
     const int  pid = threadIdx.x;
     const int elem = blockIdx.x;
 
     for (CeedInt i = pid; i < esize; i += blockDim.x) {
-        const CeedInt ind = indices ? indices[i + elem * esize] : i + elem * esize;
+        const CeedInt ind = offsets ? offsets[i + elem * esize] : i + elem * esize;
         for (CeedInt comp = 0; comp < NCOMP; ++comp) {
-            // magmablas_datomic_add(&dv[ind + esize * comp], 
-            //                       du[i+comp*esize+elem*NCOMP*esize]);
             magmablas_datomic_add(dv + (ind + compstride * comp),
                                   du[i+elem*esize+comp*esize*nelem]);
         }
@@ -115,8 +109,6 @@ magma_writeDofsStrided_kernel(const int NCOMP, const int esize, const int nelem,
 
     for (CeedInt i = pid; i < esize; i += blockDim.x) {
         for (CeedInt comp = 0; comp < NCOMP; ++comp) {
-            // magmablas_datomic_add(&dv[ind + esize * comp], 
-            //                       du[i+comp*esize+elem*NCOMP*esize]);
             magmablas_datomic_add(dv + (i * strides[0] + comp * strides[1] + 
                                         elem * strides[2]),
                                   du[i+elem*esize+comp*esize*nelem]);
@@ -127,22 +119,22 @@ magma_writeDofsStrided_kernel(const int NCOMP, const int esize, const int nelem,
 //////////////////////////////////////////////////////////////////////////////////////////
 
 // ReadDofs to device memory
-// du is L-vector, size nnodes * NCOMP
+// du is L-vector, size lsize
 // dv is E-vector, size nelem * esize * NCOMP
 extern "C" void
 magma_readDofsOffset(const magma_int_t NCOMP, const magma_int_t compstride,
                      const magma_int_t esize, const magma_int_t nelem,
-                     magma_int_t *indices, const double *du, double *dv)
+                     magma_int_t *offsets, const double *du, double *dv)
 {
     magma_int_t grid    = nelem;
     magma_int_t threads = 256;
 
     magma_readDofsOffset_kernel<<<grid, threads, 0, NULL>>>(NCOMP, compstride,
-      esize, nelem, indices, du, dv);
+      esize, nelem, offsets, du, dv);
 }
 
 // ReadDofs to device memory, strided description for L-vector
-// du is L-vector, size nnodes * NCOMP
+// du is L-vector, size lsize
 // dv is E-vector, size nelem * esize * NCOMP
 extern "C" void
 magma_readDofsStrided(const magma_int_t NCOMP, const magma_int_t esize,
@@ -158,22 +150,22 @@ magma_readDofsStrided(const magma_int_t NCOMP, const magma_int_t esize,
 
 // WriteDofs from device memory
 // du is E-vector, size nelem * esize * NCOMP
-// dv is L-vector, size nnodes * NCOMP 
+// dv is L-vector, size lsize 
 extern "C" void
 magma_writeDofsOffset(const magma_int_t NCOMP, const magma_int_t compstride,
                       const magma_int_t esize, const magma_int_t nelem,
-                      magma_int_t *indices, const double *du, double *dv)
+                      magma_int_t *offsets, const double *du, double *dv)
 {
     magma_int_t grid    = nelem;
     magma_int_t threads = 256;
 
     magma_writeDofsOffset_kernel<<<grid, threads, 0, NULL>>>(NCOMP, compstride,
-      esize, nelem, indices, du, dv);
+      esize, nelem, offsets, du, dv);
 }
 
 // WriteDofs from device memory, strided description for L-vector
 // du is E-vector, size nelem * esize * NCOMP
-// dv is L-vector, size nnodes * NCOMP 
+// dv is L-vector, size lsize
 extern "C" void
 magma_writeDofsStrided(const magma_int_t NCOMP, const magma_int_t esize,
                        const magma_int_t nelem, const int *strides,
