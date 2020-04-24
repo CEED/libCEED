486dea29594471246ec95dbf1b81626d71bbdce0
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2020-04-24 12:26:28 -0400

    update calls

--- backends/magma/magma_grad.c
+++ backends/magma/magma_grad.c
@@ -26,24 +26,24 @@ magma_grad(
     const CeedScalar *dinterp1d, const CeedScalar *dgrad1d, CeedTransposeMode tmode, 
     const CeedScalar *dU, magma_int_t u_elstride, magma_int_t u_compstride, magma_int_t u_dimstride, 
           CeedScalar *dV, magma_int_t v_elstride, magma_int_t v_compstride, magma_int_t v_dimstride, 
-    magma_int_t nelem, magma_kernel_mode_t kernel_mode, magma_queue_t queue)
+    magma_int_t nelem, magma_kernel_mode_t kernel_mode, magma_int_t* maxthreads, magma_queue_t queue)
 {
     magma_int_t launch_failed = 0;
 
     if(kernel_mode == MAGMA_KERNEL_DIM_SPECIFIC) {
         if(tmode == CEED_TRANSPOSE){
             switch(dim) {
-                case 1: launch_failed =  magma_grad_1d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, dV, v_elstride, v_compstride, nelem, queue); break;
-                case 2: launch_failed = magma_gradt_2d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, u_dimstride, dV, v_elstride, v_compstride, v_dimstride, nelem, queue); break;
-                case 3: launch_failed = magma_gradt_3d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, u_dimstride, dV, v_elstride, v_compstride, v_dimstride, nelem, queue); break;
+                case 1: launch_failed =  magma_grad_1d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, dV, v_elstride, v_compstride, nelem, maxthreads[0], queue); break;
+                case 2: launch_failed = magma_gradt_2d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, u_dimstride, dV, v_elstride, v_compstride, v_dimstride, nelem, maxthreads[1], queue); break;
+                case 3: launch_failed = magma_gradt_3d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, u_dimstride, dV, v_elstride, v_compstride, v_dimstride, nelem, maxthreads[2], queue); break;
                 default: launch_failed = 1;
             }
         }
         else{
             switch(dim) {
-                case 1: launch_failed =  magma_grad_1d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, dV, v_elstride, v_compstride, nelem, queue); break;
-                case 2: launch_failed = magma_gradn_2d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, u_dimstride, dV, v_elstride, v_compstride, v_dimstride, nelem, queue); break;
-                case 3: launch_failed = magma_gradn_3d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, u_dimstride, dV, v_elstride, v_compstride, v_dimstride, nelem, queue); break;
+                case 1: launch_failed =  magma_grad_1d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, dV, v_elstride, v_compstride, nelem, maxthreads[0], queue); break;
+                case 2: launch_failed = magma_gradn_2d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, u_dimstride, dV, v_elstride, v_compstride, v_dimstride, nelem, maxthreads[1], queue); break;
+                case 3: launch_failed = magma_gradn_3d(P, Q, ncomp, dinterp1d, dgrad1d, tmode, dU, u_elstride, u_compstride, u_dimstride, dV, v_elstride, v_compstride, v_dimstride, nelem, maxthreads[2], queue); break;
                 default: launch_failed = 1;
             }
         }
