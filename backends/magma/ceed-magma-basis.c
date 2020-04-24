42bf5e1493050bd1a61f393dac7f35d148c4af51
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2020-04-24 15:26:11 -0400

    update high-level calls

--- backends/magma/ceed-magma-basis.c
+++ backends/magma/ceed-magma-basis.c
@@ -173,7 +173,7 @@ int CeedBasisApply_Magma(CeedBasis basis, CeedInt nelem,
     // LCOV_EXCL_STOP
     CeedInt Q = Q1d;
     int eldofssize = CeedIntPow(Q, dim);
-    ierr = magma_weight(Q, dim, impl->dqweight1d, v, eldofssize, nelem, data->basis_kernel_mode, data->queue);
+    ierr = magma_weight(Q, dim, impl->dqweight1d, v, eldofssize, nelem, data->basis_kernel_mode, data->maxthreads, data->queue);
     if(ierr != 0) CeedError(ceed, 1, "MAGMA: launch failure detected for magma_weight");
   }
   break;
