356fbf4ba22e2c8340b1c6d0f1580641103da2c0
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2020-05-02 04:42:23 -0600

    Fluids: Set up boundary for 3d problems

--- examples/fluids/setup-boundary.h
+++ examples/fluids/setup-boundary.h
@@ -28,6 +28,47 @@
 // TODO: Comment on this QFunction
 //
 // *****************************************************************************
+CEED_QFUNCTION(SetupBoundary)(void *ctx, CeedInt Q,
+                      const CeedScalar *const *in, CeedScalar *const *out) {
+  // Inputs
+  const CeedScalar (*J)[3][CEED_Q_VLA] = (const CeedScalar(*)[3][CEED_Q_VLA])in[0],
+                   (*w) = in[1];
+  // Outputs
+  CeedScalar *qdata = out[0];
+
+  CeedPragmaSIMD
+  // Quadrature Point Loop
+  for (CeedInt i=0; i<Q; i++) {
+    // Setup
+    const CeedScalar dxdX[3][2] = {{J[0][0][i],
+                                    J[1][0][i]},
+                                   {J[0][1][i],
+                                    J[1][1][i]},
+                                   {J[0][2][i],
+                                    J[1][2][i]}
+                                   };
+
+    // J is given by the cross product of the columns of dxdX
+    const CeedScalar J[3][1] = {{dxdX[1][0]*dxdX[2][1] - dxdX[2][0]*dxdX[1][1]},
+                                {dxdX[2][0]*dxdX[0][1] - dxdX[0][0]*dxdX[2][1]},
+                                {dxdX[0][0]*dxdX[1][1] - dxdX[1][0]*dxdX[0][1]}
+                               };
+
+    const CeedScalar detJ = sqrt(J[0][0]*J[0][0] +
+                                 J[1][0]*J[1][0] +
+                                 J[2][0]*J[2][0]);
+    qdata[i+Q*0] = detJ * w[i];
+
+  } // End of Quadrature Point Loop
+
+  // Return
+  return 0;
+}
+
+// *****************************************************************************
+// TODO: Comment on this QFunction
+//
+// *****************************************************************************
 CEED_QFUNCTION(SetupBoundary2d)(void *ctx, CeedInt Q,
                       const CeedScalar *const *in, CeedScalar *const *out) {
   // Inputs
