62e9c00619542b93f9507c25cbcc7b72aa74279f
Jeremy L Thompson <thompson.jeremy.luke@gmail.com>
2020-06-23 14:11:55 -0600

    Solids - enable use of GPU vectors

--- examples/solids/src/setuplibceed.c
+++ examples/solids/src/setuplibceed.c
@@ -413,7 +413,7 @@ PetscErrorCode SetupLibceedFineLevel(DM dm, DM dmEnergy, DM dmDiagnostic,
 
     // -- Compute forcing term
     CeedOperatorApply(opSetupForce, xcoord, forceCeed, CEED_REQUEST_IMMEDIATE);
-    CeedVectorSyncArray(forceCeed, CEED_MEM_HOST);
+    CeedVectorSyncArray(forceCeed, appCtx->memTypeRequested);
 
     // -- Cleanup
     CeedQFunctionDestroy(&qfSetupForce);
