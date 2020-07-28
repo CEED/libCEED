e9e8bf9009e12a0190cd4f510dfb9c43805947e1
Jeremy L Thompson <thompson.jeremy.luke@gmail.com>
2020-07-28 10:24:42 -0600

    Tests - use qfunction headers for Fortran tests to improve bypass gcov issue

--- tests/t533-operator-f.f90
+++ tests/t533-operator-f.f90
@@ -1,33 +1,8 @@
 !-----------------------------------------------------------------------
-      subroutine setup(ctx,q,u1,u2,u3,u4,u5,u6,u7,u8,u9,u10,u11,u12,u13,u14,&
-&           u15,u16,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,ierr)
-      real*8 ctx
-      real*8 u1(1)
-      real*8 u2(1)
-      real*8 v1(1)
-      integer q,ierr
-
-      do i=1,q
-        v1(i)=u1(i)*(u2(i+q*0)*u2(i+q*3)-u2(i+q*1)*u2(i+q*2))
-      enddo
-
-      ierr=0
-      end
-!-----------------------------------------------------------------------
-      subroutine mass(ctx,q,u1,u2,u3,u4,u5,u6,u7,u8,u9,u10,u11,u12,u13,u14,&
-&           u15,u16,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,ierr)
-      real*8 ctx
-      real*8 u1(1)
-      real*8 u2(1)
-      real*8 v1(1)
-      integer q,ierr
-
-      do i=1,q
-        v1(i)=u2(i)*u1(i)
-      enddo
-
-      ierr=0
-      end
+!
+! Header with QFunctions
+! 
+      include 't510-operator-f.h'
 !-----------------------------------------------------------------------
       program test
       implicit none
