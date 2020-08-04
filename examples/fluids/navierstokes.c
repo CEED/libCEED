10cda156c89a3278c805485b194ab7af0a7a86f7
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2020-08-04 16:58:27 -0600

    current time added to User struct

--- examples/fluids/navierstokes.c
+++ examples/fluids/navierstokes.c
@@ -250,6 +250,7 @@ struct User_ {
   Vec M;
   char outputfolder[PETSC_MAX_PATH_LEN];
   PetscInt contsteps;
+  PetscReal currentTime;
 };
 
 struct Units_ {
@@ -615,6 +616,7 @@ static PetscErrorCode RHS_NS(TS ts, PetscReal t, Vec Q, Vec G, void *userData) {
 
   // Global-to-local
   PetscFunctionBeginUser;
+  user->currentTime = t;
   ierr = DMGetLocalVector(user->dm, &Qloc); CHKERRQ(ierr);
   ierr = DMGetLocalVector(user->dm, &Gloc); CHKERRQ(ierr);
   ierr = VecZeroEntries(Qloc); CHKERRQ(ierr);
@@ -1315,7 +1317,7 @@ int main(int argc, char **argv) {
     .wind[0] = wind[0],
     .wind[1] = wind[1],
     .wind[2] = wind[2],
-    .time = 0,
+    .time = user->currentTime,
     .vortex_strength = vortex_strength,
     .wind_type = wind_type,
   };
