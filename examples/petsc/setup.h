23469acc68d9f2a98f561c7b5de6ee2a3745201a
valeriabarra <valeriabarra21@gmail.com>
2020-08-04 10:29:06 -0600

    Add compatibility macros

--- examples/petsc/setup.h
+++ examples/petsc/setup.h
@@ -43,6 +43,10 @@
 #  define DMPlexRestoreClosureIndices(a,b,c,d,e,f,g,h,i) DMPlexRestoreClosureIndices(a,b,c,d,f,g,i)
 #endif
 
+#if PETSC_VERSION_LT(3,14,0)
+#  define DMAddBoundary(a,b,c,d,e,f,g,h,i,j,k,l) DMAddBoundary(a,b,c,d,e,f,g,h,j,k,l)
+#endif
+
 // -----------------------------------------------------------------------------
 // PETSc Operator Structs
 // -----------------------------------------------------------------------------
