aa6c9dab585100201df10e307b5d6ce4d961c247
jeremylt <thompson.jeremy.luke@gmail.com>
2020-08-06 14:14:42 -0600

    WIP - fix include path

--- rust/build.rs
+++ rust/build.rs
@@ -6,7 +6,7 @@ use std::path::PathBuf;
 fn main() {
     // Tell cargo to tell rustc to link the system bzip2
     // shared library.
-    println!("cargo:rustc-link-lib=ceed");
+    println!("cargo:rustc-link-search=native={}", "../lib");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.h");
