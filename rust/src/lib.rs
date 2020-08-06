bdcd576960a6f86efe7bc040eca81b280009e00e
jeremylt <thompson.jeremy.luke@gmail.com>
2020-08-06 17:14:54 -0600

    WIP - fix linking, for now

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,3 +3,20 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use std::mem;
+
+  #[test]
+  fn ceed_init() {
+    unsafe {
+      
+      let mut ceed: Ceed = libc::malloc(mem::size_of::<Ceed>()) as Ceed;
+      let resource = "/cpu/self/ref/serial";
+      CeedInit(resource.as_ptr() as *const i8, &mut  ceed);
+      CeedDestroy(&mut ceed);
+    }
+  }
+}
