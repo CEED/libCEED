11746efa39328f3952064ddc5ecf4bf8466021c0
Yohann Dudouit <dudouit1@llnl.gov>
2020-08-07 10:29:23 -0700

    Add some functions to vector

--- rust/src/vector.rs
+++ rust/src/vector.rs
@@ -6,6 +6,9 @@ pub struct Vector<'a> {
   ceed : &'a crate::Ceed,
   ptr : bind_ceed::CeedVector,
 }
+pub enum NormType {
+
+}
 impl<'a> Vector<'a> {
   pub fn create(ceed: &'a crate::Ceed, n: i32) -> Self {
     let mut ptr = unsafe { libc::malloc(mem::size_of::<bind_ceed::CeedVector>()) as bind_ceed::CeedVector };
@@ -16,6 +19,26 @@ impl<'a> Vector<'a> {
   pub fn new(ceed: &'a crate::Ceed, ptr: bind_ceed::CeedVector) -> Self {
     Self { ceed , ptr}
   }
+
+  pub fn length(&self) -> i32 {
+    let mut n = 0;
+    unsafe { bind_ceed::CeedVectorGetLength(self.ptr, &mut n) };
+    n
+  }
+
+  pub fn set_value(&mut self, value : f64) {
+    unsafe { bind_ceed::CeedVectorSetValue(self.ptr, value) };
+  }
+
+  // pub fn sync(&self, mtype: crate::MemType) {
+  //   unsafe { bind_ceed::CeedVectorSyncArray(self.ptr, mtype) };
+  // }
+
+  // pub fn norm(&self, type: NormType)  -> f64 {
+  //   let res :f64 = 0.0;
+  //   unsafe{ bind_ceed::CeedVectorNorm(self.ptr, type, &mut res) };
+  //   res
+  // }
 }
 
 /// Destructor
