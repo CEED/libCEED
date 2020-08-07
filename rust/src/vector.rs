4476458311a4433abb0db2956c405c058d3f3a97
Yohann Dudouit <dudouit1@llnl.gov>
2020-08-07 02:28:32 -0700

    Rewrite vector constructor.

--- rust/src/vector.rs
+++ rust/src/vector.rs
@@ -1,12 +1,18 @@
 use crate::prelude::*;
+use std::mem;
 
 /// CeedVector context wrapper
 pub struct Vector<'a> {
   ceed_reference : &'a crate::Ceed,
   ceed_vec_ptr : rust_ceed::CeedVector,
 }
-
 impl<'a> Vector<'a> {
+  pub fn create(ceed_reference: &'a crate::Ceed, n: i32) -> Self {
+    let mut ceed_vec_ptr = unsafe { libc::malloc(mem::size_of::<rust_ceed::CeedVector>()) as rust_ceed::CeedVector };
+    unsafe { rust_ceed::CeedVectorCreate(ceed_reference.ceed_ptr, n, &mut ceed_vec_ptr) };
+    Self { ceed_reference , ceed_vec_ptr}
+  }
+
   pub fn new(ceed_reference: &'a crate::Ceed, ceed_vec_ptr: rust_ceed::CeedVector) -> Self {
     Self { ceed_reference , ceed_vec_ptr}
   }
