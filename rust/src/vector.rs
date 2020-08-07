24dd847855edce4592370ceb6b8154de7da6c071
Yohann Dudouit <dudouit1@llnl.gov>
2020-08-07 02:40:16 -0700

    Rename rust_ceed to bind_ceed

--- rust/src/vector.rs
+++ rust/src/vector.rs
@@ -4,16 +4,16 @@ use std::mem;
 /// CeedVector context wrapper
 pub struct Vector<'a> {
   ceed_reference : &'a crate::Ceed,
-  ceed_vec_ptr : rust_ceed::CeedVector,
+  ceed_vec_ptr : bind_ceed::CeedVector,
 }
 impl<'a> Vector<'a> {
   pub fn create(ceed_reference: &'a crate::Ceed, n: i32) -> Self {
-    let mut ceed_vec_ptr = unsafe { libc::malloc(mem::size_of::<rust_ceed::CeedVector>()) as rust_ceed::CeedVector };
-    unsafe { rust_ceed::CeedVectorCreate(ceed_reference.ceed_ptr, n, &mut ceed_vec_ptr) };
+    let mut ceed_vec_ptr = unsafe { libc::malloc(mem::size_of::<bind_ceed::CeedVector>()) as bind_ceed::CeedVector };
+    unsafe { bind_ceed::CeedVectorCreate(ceed_reference.ceed_ptr, n, &mut ceed_vec_ptr) };
     Self { ceed_reference , ceed_vec_ptr}
   }
 
-  pub fn new(ceed_reference: &'a crate::Ceed, ceed_vec_ptr: rust_ceed::CeedVector) -> Self {
+  pub fn new(ceed_reference: &'a crate::Ceed, ceed_vec_ptr: bind_ceed::CeedVector) -> Self {
     Self { ceed_reference , ceed_vec_ptr}
   }
 }
@@ -22,7 +22,7 @@ impl<'a> Vector<'a> {
 impl<'a> Drop for Vector<'a> {
   fn drop(&mut self) {
     unsafe {
-      rust_ceed::CeedVectorDestroy(&mut self.ceed_vec_ptr);
+      bind_ceed::CeedVectorDestroy(&mut self.ceed_vec_ptr);
     }
   }
 }
