334ee9a67cd1b3d778f03f56ee52bff9bd752ed6
Yohann Dudouit <dudouit1@llnl.gov>
2020-08-07 02:20:03 -0700

    fix mods in vector

--- rust/src/vector.rs
+++ rust/src/vector.rs
@@ -1,26 +1,13 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
-use std::mem;
-use std::fmt;
-use std::ffi::CString;
-use std::io::prelude::*;
-use std::io::{BufReader, SeekFrom};
-
-use cfile;
-// use std::io::{self, Write};
-
 use crate::prelude::*;
 
 /// CeedVector context wrapper
 pub struct Vector<'a> {
-  ceed_reference : &'a crate::ceed::Ceed,
+  ceed_reference : &'a crate::Ceed,
   ceed_vec_ptr : rust_ceed::CeedVector,
 }
 
 impl<'a> Vector<'a> {
-  pub fn new(ceed_reference: &'a crate::ceed::Ceed, ceed_vec_ptr: rust_ceed::CeedVector) -> Self {
+  pub fn new(ceed_reference: &'a crate::Ceed, ceed_vec_ptr: rust_ceed::CeedVector) -> Self {
     Self { ceed_reference , ceed_vec_ptr}
   }
 }
