0c068e6bc01d33ab8dd4c02ec44b71ca65018c90
Yohann Dudouit <dudouit1@llnl.gov>
2020-09-09 19:18:19 -0700

    revert add.

--- backends/cuda-shared/ceed-cuda-shared-basis.c
+++ backends/cuda-shared/ceed-cuda-shared-basis.c
@@ -26,10 +26,8 @@ static const char *kernelsShared = QUOTE(
 // Sum input into output
 //------------------------------------------------------------------------------
 inline __device__ void add(CeedScalar *r_V, const CeedScalar *r_U) {
-  if (tidx < P1D && tidy < P1D) {
-    for (int i = 0; i < Q1D; i++)
-      r_V[i] += r_U[i];
-  }
+  for (int i = 0; i < Q1D; i++)
+    r_V[i] += r_U[i];
 }
 
 //------------------------------------------------------------------------------
