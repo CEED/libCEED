9b4778b6c48d707612411a5cc86d644c94ccac59
jeremylt <thompson.jeremy.luke@gmail.com>
2020-09-29 09:47:05 -0600

    Vector - add reader check to destructor

--- interface/ceed-vector.c
+++ interface/ceed-vector.c
@@ -574,9 +574,13 @@ int CeedVectorDestroy(CeedVector *vec) {
 
   if (!*vec || --(*vec)->refcount > 0) return 0;
 
-  if ((*vec) && ((*vec)->state % 2) == 1)
-    return CeedError((*vec)->ceed, 1, "Cannot destroy CeedVector, the access "
+  if (((*vec)->state % 2) == 1)
+    return CeedError((*vec)->ceed, 1, "Cannot destroy CeedVector, the writable access "
                      "lock is in use");
+
+  if ((*vec)->numreaders > 0)
+    return CeedError((*vec)->ceed, 1, "Cannot destroy CeedVector, a process has "
+                     "read access");
 
   if ((*vec)->Destroy) {
     ierr = (*vec)->Destroy(*vec); CeedChk(ierr);
