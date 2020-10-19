93fb411df7c219adbf6df6da19221bca352404bf
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2020-10-18 19:35:08 -0600

    commented the error handlers and moved petsc options regarding the boundary conditions to each compilation unit

--- examples/fluids/navierstokes.h
+++ examples/fluids/navierstokes.h
@@ -67,9 +67,9 @@ typedef struct {
   CeedQFunctionUser setupVol, setupSur, ics, applyVol_rhs, applyVol_ifunction,
                     applySur;
   const char *setupVol_loc, *setupSur_loc, *ics_loc, *applyVol_rhs_loc,
-        *applyVol_ifunction_loc, *applySur_loc;
+             *applyVol_ifunction_loc, *applySur_loc;
   bool non_zero_time;
-  PetscErrorCode (*bc_func)(DM, MPI_Comm, SimpleBC, void *);
+  PetscErrorCode (*bc)(DM, SimpleBC, void *);
 } problemData;
 
 // PETSc user data
@@ -114,8 +114,8 @@ extern PetscErrorCode NS_ADVECTION(problemData *problem);
 extern PetscErrorCode NS_ADVECTION2D(problemData *problem);
 
 // Boundary Condition Functions
-extern PetscErrorCode BC_DENSITY_CURRENT(DM dm, MPI_Comm comm, SimpleBC bc, void *ctxSetupData);
-extern PetscErrorCode BC_ADVECTION(DM dm, MPI_Comm comm, SimpleBC bc, void *ctxSetupData);
-extern PetscErrorCode BC_ADVECTION2D(DM dm, MPI_Comm comm, SimpleBC bc, void *ctxSetupData);
+extern PetscErrorCode BC_DENSITY_CURRENT(DM dm, SimpleBC bc, void *ctxSetupData);
+extern PetscErrorCode BC_ADVECTION(DM dm, SimpleBC bc, void *ctxSetupData);
+extern PetscErrorCode BC_ADVECTION2D(DM dm, SimpleBC bc, void *ctxSetupData);
 
 #endif
