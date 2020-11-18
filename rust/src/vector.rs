246d46e653a9dc544ff5c328ccbd3da5aa996f34
jeremylt <thompson.jeremy.luke@gmail.com>
2020-11-18 08:29:54 -0700

    formatting

--- rust/src/vector.rs
+++ rust/src/vector.rs
@@ -14,7 +14,7 @@
 // software, applications, hardware, advanced system engineering and early
 // testbed platforms, in support of the nation's exascale computing imperative
 
-//! A Ceed Vector constitutes the main data structure and serveS as input/output
+//! A Ceed Vector constitutes the main data structure and serves as input/output
 //! for Ceed Operators.
 
 use std::{
