b16cea3de2f2e9577793c94ab23ce566b45fb1ca
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2020-12-09 22:07:29 -0700

    examples/fluids: fixed a typo in all examples

--- examples/fluids/euler-vortex.h
+++ examples/fluids/euler-vortex.h
@@ -46,7 +46,7 @@ struct EulerContext_ {
 #endif
 
 // *****************************************************************************
-// These function sets the the initial conditions and boundary conditions
+// This function sets the initial conditions and the boundary conditions
 //
 // These initial conditions are given in terms of potential temperature and
 //   Exner pressure and then converted to density and total energy.
