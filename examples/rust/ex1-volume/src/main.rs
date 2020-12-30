52e3e3aa6d5337c40a294e3946bd7f7156bf2021
jeremylt <thompson.jeremy.luke@gmail.com>
2020-12-30 10:00:36 -0700

    Rust - update year in header

--- examples/rust/ex1-volume/src/main.rs
+++ examples/rust/ex1-volume/src/main.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018, Lawrence Livermore National Security, LLC.
+// Copyright (c) 2017-2021, Lawrence Livermore National Security, LLC.
 // Produced at the Lawrence Livermore National Laboratory. LLNL-CODE-734707.
 // All Rights reserved. See files LICENSE and NOTICE for details.
 //
