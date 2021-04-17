7e780f740a074678493c134f1f9a6a42322e0a42
Jed Brown <jed@jedbrown.org>
2021-04-16 22:30:29 -0600

    doc: update intersphinx link for Python to squash warning

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -272,7 +272,7 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'python': ('https://docs.python.org', None),
+    'python': ('https://docs.python.org/3', None),
     'numpy': ('https://numpy.org/devdocs', None),
 }
 
