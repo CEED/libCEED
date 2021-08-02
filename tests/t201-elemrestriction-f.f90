0acb07cd915a46efccf10dc99d60f8eab5d4016d
Jeremy L Thompson <jeremy@jeremylt.org>
2021-08-02 12:27:36 -0600

    tests - refactor t202,203,208,213 to not use output

--- tests/t201-elemrestriction-f.f90
+++ tests/t201-elemrestriction-f.f90
@@ -35,8 +35,8 @@
       strides=[1,2,2]
       call ceedelemrestrictioncreatestrided(ceed,ne,2,1,2*ne,strides,r,err)
 
-      call ceedvectorcreate(ceed,2*ne,y,err);
-      call ceedvectorsetvalue(y,0.d0,err);
+      call ceedvectorcreate(ceed,2*ne,y,err)
+      call ceedvectorsetvalue(y,0.d0,err)
       call ceedelemrestrictionapply(r,ceed_notranspose,x,y,&
      & ceed_request_immediate,err)
 
