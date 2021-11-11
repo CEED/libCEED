7184d99cffbd3c39c089e6a0980320a04b1b84ee
Jeremy L Thompson <jeremy@jeremylt.org>
2021-11-11 07:49:16 -0700

    sve - initial SVE backend framework

---
+++ backends/sve/ceed-sve-blocked.c
@@ -0,0 +1,43 @@
+// Copyright (c) 2017-2022, Lawrence Livermore National Security, LLC and other CEED contributors.
+// All Rights Reserved. See the top-level LICENSE and NOTICE files for details.
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This file is part of CEED:  http://github.com/ceed
+
+#include <ceed.h>
+#include <ceed/backend.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "ceed-sve.h"
+
+//------------------------------------------------------------------------------
+// Backend Init
+//------------------------------------------------------------------------------
+static int CeedInit_Sve(const char *resource, Ceed ceed) {
+  Ceed ceed_ref;
+
+  CeedCheck(!strcmp(resource, "/cpu/self") || !strcmp(resource, "/cpu/self/sve") || !strcmp(resource, "/cpu/self/sve/blocked"), ceed,
+            CEED_ERROR_BACKEND, "SVE backend cannot use resource: %s", resource);
+  CeedCallBackend(CeedSetDeterministic(ceed, true));
+
+  // Create reference CEED that implementation will be dispatched through unless overridden
+  CeedCallBackend(CeedInit("/cpu/self/opt/blocked", &ceed_ref));
+  CeedCallBackend(CeedSetDelegate(ceed, ceed_ref));
+  CeedCallBackend(CeedDestroy(&ceed_ref));
+
+  if (CEED_SCALAR_TYPE == CEED_SCALAR_FP64) {
+    CeedCallBackend(CeedSetBackendFunction(ceed, "Ceed", ceed, "TensorContractCreate", CeedTensorContractCreate_f64_Sve));
+  } else {
+    CeedCallBackend(CeedSetBackendFunction(ceed, "Ceed", ceed, "TensorContractCreate", CeedTensorContractCreate_f32_Sve));
+  }
+  return CEED_ERROR_SUCCESS;
+}
+
+//------------------------------------------------------------------------------
+// Backend Register
+//------------------------------------------------------------------------------
+CEED_INTERN int CeedRegister_Sve_Blocked(void) { return CeedRegister("/cpu/self/sve/blocked", CeedInit_Sve, 30); }
+
+//------------------------------------------------------------------------------
