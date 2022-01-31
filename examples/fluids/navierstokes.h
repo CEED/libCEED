2680af1d34eaa10e291a596b30a30f04f39d3626
Timothy Aiken <timothyaiken@Timothys-MacBook-Pro.local>
2022-01-30 19:49:00 -0700

    Updated documentation, tau definition in SU, and recommended fixes to the flux Jacobian.

--- examples/fluids/navierstokes.h
+++ examples/fluids/navierstokes.h
@@ -259,9 +259,9 @@ struct EulerContext_ {
 #define shocktube_context_struct
 typedef struct ShockTubeContext_ *ShockTubeContext;
 struct ShockTubeContext_ {
-  CeedScalar cv;
   CeedScalar Cyzb;
   CeedScalar Byzb;
+  CeedScalar c_tau;
   bool implicit;
   bool yzb;
   int stabilization;
@@ -273,9 +273,9 @@ struct ShockTubeContext_ {
 #define shocktube_context_struct
 typedef struct ShockTubeContext_ *ShockTubeContext;
 struct ShockTubeContext_ {
-  CeedScalar cv;
   CeedScalar Cyzb;
   CeedScalar Byzb;
+  CeedScalar c_tau;
   bool implicit;
   bool yzb;
   int stabilization;
