4becc8714a1c57ed5bde8aad5b332f344c14ea1c
James Wright <james@jameswright.xyz>
2022-02-18 11:39:41 -0700

    feat(fluids): Add InterpolateProfile helper func

--- examples/fluids/qfunctions/stg_shur14.h
+++ examples/fluids/qfunctions/stg_shur14.h
@@ -30,6 +30,70 @@
 #include <stdlib.h>
 #include "../src/setupstg_shur14.h"
 
+/*
+ * @brief Interpolate quantities from input profile to given location
+ *
+ * Assumed that prof_dw[i+1] > prof_dw[i] and prof_dw[0] = 0
+ * If dw > prof_dw[-1], then the interpolation takes the values at prof_dw[-1]
+ *
+ * @param[in]  dw      Distance to the nearest wall
+ * @param[out] ubar    Mean velocity at dw
+ * @param[out] cij     Cholesky decomposition at dw
+ * @param[out] eps     Turbulent dissipation at dw
+ * @param[out] lt      Turbulent length scale at dw
+ * @param[in]  stg_ctx STGShur14Context for the problem
+ */
+void CEED_QFUNCTION_HELPER(InterpolateProfile)(const CeedScalar dw,
+    CeedScalar ubar[3], CeedScalar cij[6], CeedScalar *eps, CeedScalar *lt,
+    const STGShur14Context stg_ctx) {
+
+  CeedScalar *prof_dw = &stg_ctx->data[stg_ctx->offsets.prof_dw];
+  CeedScalar *prof_eps = &stg_ctx->data[stg_ctx->offsets.eps];
+  CeedScalar *prof_lt = &stg_ctx->data[stg_ctx->offsets.lt];
+  CeedScalar (*prof_ubar)[stg_ctx->nprofs] = (CeedScalar (
+        *)[stg_ctx->nprofs]) &stg_ctx->data[stg_ctx->offsets.ubar];
+  CeedScalar (*prof_cij)[stg_ctx->nprofs] = (CeedScalar (*)[stg_ctx->nprofs])
+      &stg_ctx->data[stg_ctx->offsets.cij];
+  CeedInt idx=-1;
+
+  for(CeedInt i=0; i<stg_ctx->nprofs; i++) {
+    if (dw > prof_dw[i]) {
+      idx = i;
+      break;
+    }
+  }
+
+  if (idx > 0) { // y within the bounds of prof_dw
+    CeedScalar coeff = (dw - prof_dw[idx-1]) / (prof_dw[idx] - prof_dw[idx-1]);
+
+    //*INDENT-OFF*
+    ubar[0] = prof_ubar[0][idx-1] + coeff*( prof_ubar[0][idx] - prof_ubar[0][idx-1] );
+    ubar[1] = prof_ubar[1][idx-1] + coeff*( prof_ubar[1][idx] - prof_ubar[1][idx-1] );
+    ubar[2] = prof_ubar[2][idx-1] + coeff*( prof_ubar[2][idx] - prof_ubar[2][idx-1] );
+    cij[0]  = prof_cij[0][idx-1]  + coeff*( prof_cij[0][idx]  - prof_cij[0][idx-1] );
+    cij[1]  = prof_cij[1][idx-1]  + coeff*( prof_cij[1][idx]  - prof_cij[1][idx-1] );
+    cij[2]  = prof_cij[2][idx-1]  + coeff*( prof_cij[2][idx]  - prof_cij[2][idx-1] );
+    cij[3]  = prof_cij[3][idx-1]  + coeff*( prof_cij[3][idx]  - prof_cij[3][idx-1] );
+    cij[4]  = prof_cij[4][idx-1]  + coeff*( prof_cij[4][idx]  - prof_cij[4][idx-1] );
+    cij[5]  = prof_cij[5][idx-1]  + coeff*( prof_cij[5][idx]  - prof_cij[5][idx-1] );
+    *eps    = prof_eps[idx-1]     + coeff*( prof_eps[idx]     - prof_eps[idx-1] );
+    *lt     = prof_lt[idx-1]      + coeff*( prof_lt[idx]      - prof_lt[idx-1] );
+    //*INDENT-ON*
+  } else { // y outside bounds of prof_dw
+    ubar[0] = prof_ubar[0][stg_ctx->nprofs];
+    ubar[1] = prof_ubar[1][stg_ctx->nprofs];
+    ubar[2] = prof_ubar[2][stg_ctx->nprofs];
+    cij[0]  = prof_cij[0][stg_ctx->nprofs];
+    cij[1]  = prof_cij[1][stg_ctx->nprofs];
+    cij[2]  = prof_cij[2][stg_ctx->nprofs];
+    cij[3]  = prof_cij[3][stg_ctx->nprofs];
+    cij[4]  = prof_cij[4][stg_ctx->nprofs];
+    cij[5]  = prof_cij[5][stg_ctx->nprofs];
+    *eps    = prof_eps[stg_ctx->nprofs];
+    *lt     = prof_lt[stg_ctx->nprofs];
+  }
+}
+
 /******************************************************
  * @brief Calculate u(x,t) for STG inflow condition
  *
