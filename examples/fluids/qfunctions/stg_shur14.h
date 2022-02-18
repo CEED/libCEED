d024e0ef7107c18d61e491c16d1e69eaded76997
James Wright <james@jameswright.xyz>
2022-02-18 11:38:17 -0700

    feat(fluids): Complete STGShur14_Calc

--- examples/fluids/qfunctions/stg_shur14.h
+++ examples/fluids/qfunctions/stg_shur14.h
@@ -33,17 +33,41 @@
 /******************************************************
  * @brief Calculate u(x,t) for STG inflow condition
  *
- * @param[in] x Location to evaluate u(x,t), [3]
- * @param[in] t Time to evaluate u(x,t)
- * @param[in] qn Wavemode amplitudes at x, [nmodes]
- * @param[out] u Velocity at x and t
+ * @param[in]  X       Location to evaluate u(X,t)
+ * @param[in]  t       Time to evaluate u(X,t)
+ * @param[in]  ubar    Mean velocity at X
+ * @param[in]  cij     Cholesky decomposition at X
+ * @param[in]  qn      Wavemode amplitudes at X, [nmodes]
+ * @param[out] u       Velocity at X and t
+ * @param[in]  stg_ctx STGShur14Context for the problem
  */
-void CEED_QFUNCTION_HELPER(STGShur14_Calc)(const CeedScalar x[3],
-    const CeedScalar t, const CeedScalar qn, CeedScalar *u[3],
+void CEED_QFUNCTION_HELPER(STGShur14_Calc)(const CeedScalar X[3],
+    const CeedScalar t, const CeedScalar ubar[3], const CeedScalar cij[6],
+    const CeedScalar qn[], CeedScalar u[3],
     const STGShur14Context stg_ctx) {
-  for (CeedInt i=0; i<3; i++) {
-    *u[i] = (CeedScalar)i;
+
+  //*INDENT-OFF*
+  CeedScalar *kappa = &stg_ctx->data[stg_ctx->offsets.kappa];
+  CeedScalar *phi   = &stg_ctx->data[stg_ctx->offsets.phi];
+  CeedScalar (*sigma)[stg_ctx->nprofs] = (CeedScalar (*)[stg_ctx->nprofs])
+                                         &stg_ctx->data[stg_ctx->offsets.sigma];
+  CeedScalar (*d)[stg_ctx->nprofs]     = (CeedScalar (*)[stg_ctx->nprofs])
+                                         &stg_ctx->data[stg_ctx->offsets.d];
+  //*INDENT-ON*
+  CeedScalar xdotd, vp[] = {0., 0., 0.};
+  CeedScalar xhat[] = {0., X[1], X[2]};
+
+  CeedPragmaSIMD
+  for(CeedInt n=0; n<stg_ctx->nmodes; n++) {
+    xhat[0] = (X[0] - stg_ctx->u0*t)*PetscMax(2*kappa[0]/kappa[n], 0.1);
+    xdotd = 0.;
+    for(CeedInt i=0; i<3; i++) xdotd += d[i][n]*xhat[i];
+    vp[0] += qn[n]*sigma[0][n]*cos(kappa[n]*xdotd + phi[n]);
   }
+
+  u[0] = ubar[0] + cij[0]*vp[0] + cij[3]*vp[0] + cij[4]*vp[0];
+  u[1] = ubar[1] + cij[3]*vp[1] + cij[1]*vp[1] + cij[5]*vp[1];
+  u[2] = ubar[2] + cij[4]*vp[2] + cij[5]*vp[2] + cij[2]*vp[2];
 }
 
 /********************************************************************
