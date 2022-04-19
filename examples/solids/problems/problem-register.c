04771a842f796e0a0aa4226cb53c6c912a123872
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2022-04-19 14:05:28 -0600

    examples/solids: update copyright headers

--- examples/solids/problems/problem-register.c
+++ examples/solids/problems/problem-register.c
@@ -1,3 +1,10 @@
+// Copyright (c) 2017-2022, Lawrence Livermore National Security, LLC and other CEED contributors.
+// All Rights Reserved. See the top-level LICENSE and NOTICE files for details.
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This file is part of CEED:  http://github.com/ceed
+
 #include <ceed.h>
 #include <petsc.h>
 #include "../problems/problems.h"
