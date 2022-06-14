233a8d6ad1534d2a36a7ef9d555a95f6fedfda3f
AdelekeBankole <adeleke.bankole@colorado.edu>
2022-06-13 22:06:36 -0600

    test sum of weights

--- tests/t334-basis.c
+++ tests/t334-basis.c
@@ -2,15 +2,20 @@
 /// Test that quadrature formula is transformed
 /// \test Test that quadrature formula is transformed
 #include <ceed.h>
+#include <math.h>
 
 int main(int argc, char **argv) {
-    Ceed ceed;
-    CeedInt Q = 16;
-    CeedScalar *q_weight_1d;
-    CeedBasis basis;
+    CeedInt Q = 5;
+    CeedScalar w_gauss[Q], w_ht[Q];
+    CeedScalar q_gauss[Q], q_ht[Q];
 
-    CeedTransformQuadrature();
+    CeedGaussQuadrature(Q, q_gauss, w_gauss);
+    CeedGaussHaleTrefethenQuadrature(1.4, Q, q_ht, w_ht);
+    for (CeedInt i=0; i<Q; i++)
+        printf("%g (%g) -> %g (%g)\n", q_gauss[i], w_gauss[i], q_ht[i], w_ht[i]);
 
-    CeedDestroy(&ceed)
+    CeedScalar w_sum = 0;
+    for (CeedInt i=0; i<Q; i++) w_sum += w_ht[i];
+    if (fabs(w_sum - 2) > 1e-12) printf("Unexpected sum of weights %g\n", w_sum);
     return 0;
 }
