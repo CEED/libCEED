b97eee48540c4afa73613bce95ee98ab0ab2bb60
Kris Rowe <kris.rowe@anl.gov>
2022-08-30 16:33:50 +0000

    Rewrites stabilization kernels to be compatible with OCCA.

--- examples/fluids/qfunctions/stabilization_types.h
+++ examples/fluids/qfunctions/stabilization_types.h
@@ -8,10 +8,15 @@
 #ifndef stabilization_types_h
 #define stabilization_types_h
 
-typedef enum {
-  STAB_NONE = 0,
-  STAB_SU   = 1,  // Streamline Upwind
-  STAB_SUPG = 2,  // Streamline Upwind Petrov-Galerkin
-} StabilizationType;
+// typedef enum {
+  // STAB_NONE = 0,
+  // STAB_SU   = 1, // Streamline Upwind
+  // STAB_SUPG = 2, // Streamline Upwind Petrov-Galerkin
+// } StabilizationType;
+
+typedef int StabilizationType;
+static const int STAB_NONE=0;
+static const int STAB_SU=1;
+static const int STAB_SUPG=2;
 
 #endif  // stabilization_types_h
