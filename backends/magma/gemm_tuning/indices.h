c81b412b7bb6f3db8a1c5fe96c1b97d59f6f200c
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2022-11-09 12:57:15 -0500

    constants for tuning

--- backends/magma/gemm_tuning/indices.h
+++ backends/magma/gemm_tuning/indices.h
@@ -1,3 +1,4 @@
+// for std gemm kernels
 #define RECORD_LENGTH    5
 #define M_INDEX          0
 #define N_INDEX          1
@@ -5,4 +6,9 @@
 #define N_BATCH_INDEX    3
 #define USE_MAGMA_INDEX  4
 
-
+// for rtc kernels
+#define RECORD_LENGTH_RTC 4
+#define M_INDEX_RTC       0
+#define N_INDEX_RTC       1
+#define K_INDEX_RTC       2
+#define NB_INDEX_RTC      3
