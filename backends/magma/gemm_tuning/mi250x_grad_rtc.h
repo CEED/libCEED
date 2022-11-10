4661c3a42704928960045e2e06264198e4b1fe3f
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2022-11-10 10:19:56 -0500

    switch tuning data

--- backends/magma/gemm_tuning/mi250x_grad_rtc.h
+++ backends/magma/gemm_tuning/mi250x_grad_rtc.h
@@ -2,7 +2,7 @@
 // auto-generated from data on mi250x-rocm5.2
 
 ////////////////////////////////////////////////////////////////////////////////
-std::vector< std::array<int, 4> > dgrad_n_mi250x =
+std::vector< std::array<int, 4> > dgrad_t_mi250x =
 {
   {1      , 1024   , 3      , 3      },
   {1      , 5120   , 3      , 2      },
@@ -84,7 +84,7 @@ std::vector< std::array<int, 4> > dgrad_n_mi250x =
 };
 
 ////////////////////////////////////////////////////////////////////////////////
-std::vector< std::array<int, 4> > dgrad_t_mi250x =
+std::vector< std::array<int, 4> > dgrad_c_mi250x =
 {
   {3      , 1024   , 1      , 12     },
   {3      , 5120   , 1      , 2      },
