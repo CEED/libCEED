7574393bf604c1466e508ce027aabbf725bb4fd8
Jeremy L Thompson <jeremy@jeremylt.org>
2022-12-08 13:50:27 -0700

    julia - fix typo

--- julia/LibCEED.jl/src/Basis.jl
+++ julia/LibCEED.jl/src/Basis.jl
@@ -372,7 +372,7 @@ function getinterp1d(b::Basis)
 end
 
 """
-    getgad(b::Basis)
+    getgrad(b::Basis)
 
 Get the gradient matrix of the given [`Basis`](@ref). Returns a tensor of size
 `(getdimension(b), getnumqpts(b), getnumnodes(b))`.
