7650ae9a66c1de2783569eed4c328204687c633e
Jed Brown <jed@jedbrown.org>
2022-12-23 22:38:29 -0700

    python: update wheels for supported versions of Python

--- python/make-wheels.sh
+++ python/make-wheels.sh
@@ -2,7 +2,7 @@
 
 export PYTHONUSERBASE=/io/wheel-user-base
 
-for PYBIN in /opt/python/cp3[6789]*/bin; do
+for PYBIN in /opt/python/cp3{7,8,9,10,11}*/bin; do
     $PYBIN/pip install --user --upgrade -r requirements.txt
     $PYBIN/pip wheel . -w wheelhouse/
 done
@@ -11,7 +11,7 @@ for wheel in wheelhouse/libceed*-linux*.whl; do
     auditwheel repair "$wheel" --plat ${WHEEL_PLAT} -w wheelhouse/
 done
 
-for PYBIN in /opt/python/cp3[6789]*/bin; do
+for PYBIN in /opt/python/cp3{7,8,9,10,11}*/bin; do
     $PYBIN/pip install --user --upgrade -r requirements-test.txt
     $PYBIN/pip install --user libceed --no-index --find-links wheelhouse/
     pushd python/tests
