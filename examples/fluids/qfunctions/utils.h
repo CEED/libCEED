c5a8c472092f0349d809a12ef6ac8d34c8aefb99
James Wright <james@jameswright.xyz>
2023-01-30 10:47:01 -0700

    fluids: Add linear algebra tools to utils.h
     - Some simple vector scaling and copying routines, cross prodcuts,
       N-dimensional dot products
     - Matrix operations (Matrix-vector, matrix-matrix), some generic, some
       for size 3 operations

--- examples/fluids/qfunctions/utils.h
+++ examples/fluids/qfunctions/utils.h
@@ -15,6 +15,11 @@
 #define M_PI 3.14159265358979323846
 #endif
 
+typedef enum {
+  NO_TRANSPOSE = 0,
+  TRANSPOSE    = 1,
+} UtilMatrixTranspose;
+
 CEED_QFUNCTION_HELPER CeedScalar Max(CeedScalar a, CeedScalar b) { return a < b ? b : a; }
 CEED_QFUNCTION_HELPER CeedScalar Min(CeedScalar a, CeedScalar b) { return a < b ? a : b; }
 
@@ -38,8 +43,162 @@ CEED_QFUNCTION_HELPER void ScaleN(CeedScalar *u, const CeedScalar alpha, const C
   CeedPragmaSIMD for (CeedInt i = 0; i < N; i++) { u[i] *= alpha; }
 }
 
+// @brief Set vector of length N to a value alpha
+CEED_QFUNCTION_HELPER void SetValueN(CeedScalar *u, const CeedScalar alpha, const CeedInt N) {
+  CeedPragmaSIMD for (CeedInt i = 0; i < N; i++) { u[i] = alpha; }
+}
+
+// @brief Copy N elements from x to y
+CEED_QFUNCTION_HELPER void CopyN(const CeedScalar *x, CeedScalar *y, const CeedInt N) {
+  CeedPragmaSIMD for (CeedInt i = 0; i < N; i++) { y[i] = x[i]; }
+}
+
+// @brief Copy 3x3 matrix from A to B
+CEED_QFUNCTION_HELPER void CopyMat3(const CeedScalar A[3][3], CeedScalar B[3][3]) { CopyN((const CeedScalar *)A, (CeedScalar *)B, 9); }
+
+// @brief Dot product of vectors with N elements
+CEED_QFUNCTION_HELPER CeedScalar DotN(const CeedScalar *u, const CeedScalar *v, const CeedInt N) {
+  CeedScalar output = 0;
+  CeedPragmaSIMD for (CeedInt i = 0; i < N; i++) { output += u[i] * v[i]; }
+  return output;
+}
+
 // @brief Dot product of 3 element vectors
 CEED_QFUNCTION_HELPER CeedScalar Dot3(const CeedScalar u[3], const CeedScalar v[3]) { return u[0] * v[0] + u[1] * v[1] + u[2] * v[2]; }
+
+// @brief Cross product of vectors with 3 elements
+CEED_QFUNCTION_HELPER void Cross3(const CeedScalar u[3], const CeedScalar v[3], CeedScalar w[3]) {
+  w[0] = (u[1] * v[2]) - (u[2] * v[1]);
+  w[1] = (u[2] * v[0]) - (u[0] * v[2]);
+  w[2] = (u[0] * v[1]) - (u[1] * v[0]);
+}
+
+// @brief Curl of vector given its gradient
+CEED_QFUNCTION_HELPER void Curl3(const CeedScalar gradient[3][3], CeedScalar v[3]) {
+  v[0] = gradient[2][1] - gradient[1][2];
+  v[1] = gradient[0][2] - gradient[2][0];
+  v[2] = gradient[1][0] - gradient[0][1];
+}
+
+// @brief Matrix vector product, b = Ax + b. A is NxM, x is M, b is N
+CEED_QFUNCTION_HELPER void MatVecNM(const CeedScalar *A, const CeedScalar *x, const CeedInt N, const CeedInt M, const UtilMatrixTranspose transpose_A,
+                                    CeedScalar *b) {
+  switch (transpose_A) {
+    case NO_TRANSPOSE:
+      CeedPragmaSIMD for (CeedInt i = 0; i < N; i++) { b[i] += DotN(&A[i * M], x, M); }
+      break;
+    case TRANSPOSE:
+      CeedPragmaSIMD for (CeedInt i = 0; i < M; i++) {
+        CeedPragmaSIMD for (CeedInt j = 0; j < N; j++) { b[i] += A[j * M + i] * x[j]; }
+      }
+      break;
+  }
+}
+
+// @brief 3x3 Matrix vector product  b = Ax + b.
+CEED_QFUNCTION_HELPER void MatVec3(const CeedScalar A[3][3], const CeedScalar x[3], const UtilMatrixTranspose transpose_A, CeedScalar b[3]) {
+  MatVecNM((const CeedScalar *)A, (const CeedScalar *)x, 3, 3, transpose_A, (CeedScalar *)b);
+}
+
+// @brief Matrix vector product with pointwise scaling of output.
+// @details b = DAx + b. A is NxM, x is M, b is N, D is diagonal NxN, represented by a vector of length N
+CEED_QFUNCTION_HELPER void MatVecDiagNM(const CeedScalar *A, const CeedScalar *x, const CeedScalar *D, const CeedInt N, const CeedInt M,
+                                        const UtilMatrixTranspose transpose_A, CeedScalar *b) {
+  switch (transpose_A) {
+    case NO_TRANSPOSE:
+      CeedPragmaSIMD for (CeedInt i = 0; i < N; i++) { b[i] += D[i] * DotN(&A[i * M], x, M); }
+      break;
+    case TRANSPOSE:
+      CeedPragmaSIMD for (CeedInt i = 0; i < M; i++) {
+        CeedPragmaSIMD for (CeedInt j = 0; j < N; j++) { b[i] += D[i] * A[j * M + i] * x[j]; }
+      }
+      break;
+  }
+}
+
+// @brief 3x3 Matrix vector product with pointwise scaling of output. b = DAx + b.
+// @details D is diagonal, represented by a vector of length 3
+CEED_QFUNCTION_HELPER void MatVecDiag3(const CeedScalar A[3][3], const CeedScalar x[3], const CeedScalar D[3], const UtilMatrixTranspose transpose_A,
+                                       CeedScalar b[3]) {
+  MatVecDiagNM((const CeedScalar *)A, (const CeedScalar *)x, (const CeedScalar *)D, 3, 3, transpose_A, (CeedScalar *)b);
+}
+
+// @brief Matrix-Matrix product, B = DA + B, where D is diagonal.
+// @details A is NxM, D is diagonal NxN, represented by a vector of length N, and B is NxM. Optionally, A may be transposed.
+CEED_QFUNCTION_HELPER void MatDiagNM(const CeedScalar *A, const CeedScalar *D, const CeedInt N, const CeedInt M,
+                                     const UtilMatrixTranspose transpose_A, CeedScalar *B) {
+  switch (transpose_A) {
+    case NO_TRANSPOSE:
+      CeedPragmaSIMD for (CeedInt i = 0; i < N; i++) {
+        CeedPragmaSIMD for (CeedInt j = 0; j < M; j++) { B[i * M + j] += D[i] * A[i * M + j]; }
+      }
+      break;
+    case TRANSPOSE:
+      CeedPragmaSIMD for (CeedInt i = 0; i < M; i++) {
+        CeedPragmaSIMD for (CeedInt j = 0; j < N; j++) { B[i * N + j] += D[i] * A[j * M + i]; }
+      }
+      break;
+  }
+}
+
+// @brief 3x3 Matrix-Matrix product, B = DA + B, where D is diagonal.
+// @details Optionally, A may be transposed.
+CEED_QFUNCTION_HELPER void MatDiag3(const CeedScalar A[3][3], const CeedScalar D[3], const UtilMatrixTranspose transpose_A, CeedScalar B[3][3]) {
+  MatDiagNM((const CeedScalar *)A, (const CeedScalar *)D, 3, 3, transpose_A, (CeedScalar *)B);
+}
+
+// @brief Matrix-Matrix product, B = AC + B
+// @details A is NxM, C is MxO
+CEED_QFUNCTION_HELPER void MatMatNMO(const CeedScalar *A, const CeedScalar *B, const CeedInt N, const CeedInt M, const CeedInt O, CeedScalar *C) {
+  CeedPragmaSIMD for (CeedInt i = 0; i < N; i++) {
+    CeedPragmaSIMD for (CeedInt j = 0; j < O; j++) {
+      CeedPragmaSIMD for (CeedInt k = 0; k < M; k++) { C[i * O + j] += A[i * M + k] * B[k * O + j]; }
+    }
+  }
+}
+
+// @brief 3x3 Matrix-Matrix product, C = AB + C
+CEED_QFUNCTION_HELPER void MatMat3(const CeedScalar A[3][3], const CeedScalar B[3][3], const UtilMatrixTranspose transpose_A,
+                                   const UtilMatrixTranspose transpose_B, CeedScalar C[3][3]) {
+  switch (transpose_A) {
+    case NO_TRANSPOSE:
+      switch (transpose_B) {
+        case NO_TRANSPOSE:
+          CeedPragmaSIMD for (CeedInt i = 0; i < 3; i++) {
+            CeedPragmaSIMD for (CeedInt j = 0; j < 3; j++) {
+              CeedPragmaSIMD for (CeedInt k = 0; k < 3; k++) { C[i][j] += A[i][k] * B[k][j]; }
+            }
+          }
+          break;
+        case TRANSPOSE:
+          CeedPragmaSIMD for (CeedInt i = 0; i < 3; i++) {
+            CeedPragmaSIMD for (CeedInt j = 0; j < 3; j++) {
+              CeedPragmaSIMD for (CeedInt k = 0; k < 3; k++) { C[i][j] += A[i][k] * B[j][k]; }
+            }
+          }
+          break;
+      }
+      break;
+    case TRANSPOSE:
+      switch (transpose_B) {
+        case NO_TRANSPOSE:
+          CeedPragmaSIMD for (CeedInt i = 0; i < 3; i++) {
+            CeedPragmaSIMD for (CeedInt j = 0; j < 3; j++) {
+              CeedPragmaSIMD for (CeedInt k = 0; k < 3; k++) { C[i][j] += A[k][i] * B[k][j]; }
+            }
+          }
+          break;
+        case TRANSPOSE:
+          CeedPragmaSIMD for (CeedInt i = 0; i < 3; i++) {
+            CeedPragmaSIMD for (CeedInt j = 0; j < 3; j++) {
+              CeedPragmaSIMD for (CeedInt k = 0; k < 3; k++) { C[i][j] += A[k][i] * B[j][k]; }
+            }
+          }
+          break;
+      }
+      break;
+  }
+}
 
 // @brief Unpack Kelvin-Mandel notation symmetric tensor into full tensor
 CEED_QFUNCTION_HELPER void KMUnpack(const CeedScalar v[6], CeedScalar A[3][3]) {
@@ -50,6 +209,24 @@ CEED_QFUNCTION_HELPER void KMUnpack(const CeedScalar v[6], CeedScalar A[3][3]) {
   A[2][1] = A[1][2] = weight * v[3];
   A[2][0] = A[0][2] = weight * v[4];
   A[1][0] = A[0][1] = weight * v[5];
+}
+
+// @brief Pack full tensor into Kelvin-Mandel notation symmetric tensor
+CEED_QFUNCTION_HELPER void KMPack(const CeedScalar A[3][3], CeedScalar v[6]) {
+  const CeedScalar weight = sqrt(2.);
+  v[0]                    = A[0][0];
+  v[1]                    = A[1][1];
+  v[2]                    = A[2][2];
+  v[3]                    = A[2][1] * weight;
+  v[4]                    = A[2][0] * weight;
+  v[5]                    = A[1][0] * weight;
+}
+
+// @brief Calculate metric tensor from mapping, g_{ij} = xi_{k,i} xi_{k,j} = dXdx^T dXdx
+CEED_QFUNCTION_HELPER void KMMetricTensor(const CeedScalar dXdx[3][3], CeedScalar km_g_ij[6]) {
+  CeedScalar g_ij[3][3] = {{0.}};
+  MatMat3(dXdx, dXdx, TRANSPOSE, NO_TRANSPOSE, g_ij);
+  KMPack(g_ij, km_g_ij);
 }
 
 // @brief Linear ramp evaluation
