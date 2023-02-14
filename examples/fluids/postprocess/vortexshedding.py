afce146ba2ee5ff99f6b492f64ccb84bcb373ddc
Jed Brown <jed@jedbrown.org>
2023-02-13 23:57:45 -0700

    examples/fluids/postprocess: put shedding period in title

--- examples/fluids/postprocess/vortexshedding.py
+++ examples/fluids/postprocess/vortexshedding.py
@@ -10,19 +10,26 @@ def coeff(force, rho=1, u=1, D=1, zspan=0.2):
     return 2 * force / (rho * u**2 * S)
 
 
+def shedding_period(df):
+    sample = df[df["Time"] > 70]  # once the initial transient has passed
+    peaks, _ = sig.find_peaks(sample["ForceY"])
+    period = np.diff(sample["Time"].iloc[peaks])
+    return period.mean()
+
+
 df = pandas.read_csv("force.csv")
 df["Drag Coefficient"] = coeff(df["ForceX"])
 df["Lift Coefficient"] = coeff(df["ForceY"])
+period = shedding_period(df)
 
 sns.set_theme(style="ticks")
 palette = sns.color_palette()
 fig, ax_drag = plt.subplots()
 ax_lift = ax_drag.twinx()
 
-sns.lineplot(data=df, x="Time", y="Drag Coefficient",
-             ax=ax_drag, color=palette[0])
-sns.lineplot(data=df, x="Time", y="Lift Coefficient",
-             ax=ax_lift, color=palette[1])
+sns.lineplot(data=df, x="Time", y="Drag Coefficient", ax=ax_drag, color=palette[0])
+sns.lineplot(data=df, x="Time", y="Lift Coefficient", ax=ax_lift, color=palette[1])
+ax_drag.set_title(f"Shedding period {period}")
 ax_drag.set_ylim(0.41, 0.48)
 ax_drag.tick_params(axis="y", colors=palette[0])
 ax_drag.yaxis.label.set_color(palette[0])
@@ -30,11 +37,3 @@ ax_lift.tick_params(axis="y", colors=palette[1])
 ax_lift.yaxis.label.set_color(palette[1])
 
 plt.show()
-
-
-# compute and print shedding period
-sample = df[df['Time'] > 70]    # once the initial transient has passed
-peaks, _ = sig.find_peaks(sample['ForceY'])
-period = np.diff(sample['Time'].iloc[peaks])
-print(period)  # [5.55 5.6  5.55 5.55]
-print(period.mean())  # 5.5625
