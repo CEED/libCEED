9eda29bbadec418ac28756665e7ecfbef6ab83b6
AdelekeBankole <adeleke.bankole@colorado.edu>
2023-03-13 23:11:55 -0600

    create qfunction file

---
+++ examples/fluids/qfunctions/wall_dist_func.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2017-2023, Lawrence Livermore National Security, LLC and other CEED contributors.
+// All Rights Reserved. See the top-level LICENSE and NOTICE files for details.
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This file is part of CEED:  http://github.com/ceed
+
+#include <ceed.h>
+
+#include "newtonian_state.h"
+#include "utils.h"
