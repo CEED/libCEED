00ea50ebb9d42c6216393707c323d48e7ad304be
AdelekeBankole <adeleke.bankole@colorado.edu>
2023-03-14 20:50:25 -0600

    update qfunction file

--- examples/fluids/qfunctions/wall_dist_func.h
+++ examples/fluids/qfunctions/wall_dist_func.h
@@ -5,7 +5,26 @@
 //
 // This file is part of CEED:  http://github.com/ceed
 
+#ifndef wall_dist_func_h
+#define wall_dist_func_h
+
 #include <ceed.h>
 
 #include "newtonian_state.h"
 #include "utils.h"
+
+CEED_QFUNCTION_HELPER int DistanceFunction(void *ctx, CeedInt Q, const CeedScalar *const *in, CeedScalar *const *out) {
+  // inputs
+  const CeedScalar(*q)[CEED_Q_VLA]      = (const CeedScalar(*)[CEED_Q_VLA])in[0];
+  const CeedScalar(*q_data)[CEED_Q_VLA] = (const CeedScalar(*)[CEED_Q_VLA])in[1];
+  const CeedScalar(*x)[CEED_Q_VLA]      = (const CeedScalar(*)[CEED_Q_VLA])in[2];
+  //  Outputs
+  CeedScalar(*v)[CEED_Q_VLA]  = (CeedScalar(*)[CEED_Q_VLA])out[0];
+  CeedScalar(*dv)[CEED_Q_VLA] = (CeedScalar(*)[CEED_Q_VLA])out[1];
+
+  NewtonianIdealGasContext context = (NewtonianIdealGasContext)ctx;
+
+  return 0;
+}
+
+#endif  // wall_dist_func_h
