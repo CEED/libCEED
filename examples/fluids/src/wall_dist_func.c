55e1d1e9e681965b1e4a5906e7b8a6ffcb225f69
AdelekeBankole <adeleke.bankole@colorado.edu>
2023-03-14 20:51:55 -0600

    create and add qfunction fields

--- examples/fluids/src/wall_dist_func.c
+++ examples/fluids/src/wall_dist_func.c
@@ -7,7 +7,7 @@
 
 /// @file
 /// General wall distance functions for Navier-Stokes example using PETSc
-/// We do this by solving the Poisson equation ∇^{2} φ  = -1
+/// We do this by solving the Poisson equation ∇^{2} φ  = -1 with weak form ∫ ∇v ⋅ ∇ φ - v
 
 #include "../qfunctions/wall_dist_func.h"
 
@@ -16,10 +16,18 @@
 
 // General distance functions
 static PetscErrorCode Distance_Function_NS(DM dm, User user) {
-  DM       dmDist;
-  PetscFE  fe;
-  PetscInt dim = 3;
-  SNES     snesDist;
+  PetscScalar  *r;
+  DM            dmDist;
+  PetscFE       fe;
+  PetscInt     *xl_size, dim = 3;
+  PetscInt      distance_function, distance_function_loc;
+  SNES          snesDist;
+  Vec          *X_loc, rhs, rhs_loc;
+  PetscMemType  mem_type;
+  Ceed          ceed;
+  CeedVector    rhs_ceed;
+  CeedOperator  op_distance_function;
+  CeedQFunction qf_distance_function;
 
   PetscFunctionBeginUser;
   PetscCall(PetscFECreateDefault(PETSC_COMM_SELF, dim, 1, PETSC_FALSE, NULL, PETSC_DETERMINE, &fe));
@@ -29,6 +37,29 @@ static PetscErrorCode Distance_Function_NS(DM dm, User user) {
   PetscObjectSetOptionsPrefix((PetscObject)snesDist, "distance_");
   PetscCall(DMClone(dm, &dmDist));
   PetscCall(DMAddField(dmDist, NULL, (PetscObject)fe));
+
+  // Create RHS vector
+  PetscCall(VecDuplicate(X_loc, &rhs_loc));
+  PetscCall(VecZeroEntries(rhs_loc));
+  PetscCall(VecGetArrayAndMemType(rhs_loc, &r, &mem_type));
+  CeedVectorCreate(ceed, xl_size, &rhs_ceed);
+  CeedVectorSetArray(rhs_ceed, MemTypeP2C(mem_type), CEED_USE_POINTER, r);
+
+  // Gather RHS
+  CeedVectorTakeArray(rhs_ceed, MemTypeP2C(mem_type), NULL);
+  PetscCall(VecRestoreArrayAndMemType(rhs_loc, &r));
+  PetscCall(VecZeroEntries(rhs));
+  PetscCall(DMLocalToGlobal(dmDist, rhs_loc, ADD_VALUES, rhs));
+  CeedVectorDestroy(&rhs_ceed);
+
+  // Create and Add QFunction fields
+  CeedQFunctionCreateInterior(ceed, dim, distance_function, distance_function_loc, &qf_distance_function);
+  CeedQFunctionAddInput(qf_distance_function, "v", dim, CEED_EVAL_INTERP);
+  CeedQFunctionAddInput(qf_distance_function, "dphi", dim, CEED_EVAL_GRAD);
+  CeedQFunctionAddInput(qf_distance_function, "q_data", dim * (dim + 1) / 2, CEED_EVAL_NONE);
+  CeedQFunctionAddOutput(qf_distance_function, "dv", dim, CEED_EVAL_GRAD);
+
+  CeedOperatorCreate(ceed, qf_distance_function, CEED_QFUNCTION_NONE, CEED_QFUNCTION_NONE, &op_distance_function);
 
   PetscFunctionReturn(0);
 }
