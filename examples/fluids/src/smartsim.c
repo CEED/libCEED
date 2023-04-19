3ca6f1f2561159125e57ec2b363a50460e459fe3
James Wright <james@jameswright.xyz>
2023-04-19 11:00:42 -0600

    fluids: Add smartsim.c file (probably should be squashed)

---
+++ examples/fluids/src/smartsim.c
@@ -0,0 +1,11 @@
+// Copyright (c) 2017-2023, Lawrence Livermore National Security, LLC and other CEED contributors.
+// All Rights Reserved. See the top-level LICENSE and NOTICE files for details.
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This file is part of CEED:  http://github.com/ceed
+// Based on the instructions from https://www.craylabs.org/docs/sr_integration.html and PHASTA implementation
+
+
+#include <sr_enums.h>
+#include <client.h>
