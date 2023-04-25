ca41b51b6ecadda62b3d78f5f9f77ec4e4db28ea
James Wright <james@jameswright.xyz>
2023-04-25 11:00:01 -0600

    fluids: Move smartsim things to header file

--- examples/fluids/src/smartsim.c
+++ examples/fluids/src/smartsim.c
@@ -6,33 +6,9 @@
 // This file is part of CEED:  http://github.com/ceed
 // Based on the instructions from https://www.craylabs.org/docs/sr_integration.html and PHASTA implementation
 
-#include <c_client.h>
-#include <sr_enums.h>
+#include "../include/smartsim.h"
 
 #include "../navierstokes.h"
-
-#if defined(__clang_analyzer__)
-void SmartRedisCall(SRError);
-#else
-#define SmartRedisCall(...)                                                                                                           \
-  do {                                                                                                                                \
-    SRError ierr_smartredis_call_q_;                                                                                                  \
-    PetscStackUpdateLine;                                                                                                             \
-    ierr_smartredis_call_q_ = __VA_ARGS__;                                                                                            \
-    if (PetscUnlikely(ierr_smartredis_call_q_ != SRNoError))                                                                          \
-      SETERRQ(PETSC_COMM_SELF, ierr_smartredis_call_q_, "SmartRedis Error (Code %d): %s", ierr_smartredis_call_q_, SRGetLastError()); \
-  } while (0)
-#endif
-
-static PetscErrorCode SmartRedisVerifyPutTensor(void *c_client, const char *name, const size_t name_length) {
-  bool does_exist = false;
-  PetscFunctionBeginUser;
-
-  SmartRedisCall(tensor_exists(c_client, name, name_length, &does_exist));
-  PetscCheck(does_exist, PETSC_COMM_SELF, -1, "Tensor of name '%s' was not written to the database successfully", name);
-
-  PetscFunctionReturn(0);
-}
 
 PetscErrorCode SmartSimTrainingSetup(User user) {
   SmartSimData smartsim = user->smartsim;
