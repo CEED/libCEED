f9d8149fdc6c5aab2c495ba135c59631063a7b19
James Wright <james@jameswright.xyz>
2023-06-07 07:47:30 -0600

    fluids: Store diff filter field ids

--- examples/fluids/src/differential_filter.c
+++ examples/fluids/src/differential_filter.c
@@ -206,14 +206,16 @@ PetscErrorCode DifferentialFilterSetup(Ceed ceed, User user, CeedData ceed_data,
       PetscCall(DMSetupByOrder_FEM(PETSC_TRUE, PETSC_TRUE, user->app_ctx->degree, 1, user->app_ctx->q_extra, diff_filter->num_filtered_fields,
                                    diff_filter->num_field_components, diff_filter->dm_filter));
 
+      diff_filter->field_prim_state = 0;
+      diff_filter->field_velo_prod  = 1;
       PetscCall(DMGetLocalSection(diff_filter->dm_filter, &section));
-      PetscCall(PetscSectionSetFieldName(section, 0, "Filtered Primitive State Variables"));
+      PetscCall(PetscSectionSetFieldName(section, diff_filter->field_prim_state, "Filtered Primitive State Variables"));
       PetscCall(PetscSectionSetComponentName(section, 0, DIFF_FILTER_PRESSURE, "FilteredPressure"));
       PetscCall(PetscSectionSetComponentName(section, 0, DIFF_FILTER_VELOCITY_X, "FilteredVelocityX"));
       PetscCall(PetscSectionSetComponentName(section, 0, DIFF_FILTER_VELOCITY_Y, "FilteredVelocityY"));
       PetscCall(PetscSectionSetComponentName(section, 0, DIFF_FILTER_VELOCITY_Z, "FilteredVelocityZ"));
       PetscCall(PetscSectionSetComponentName(section, 0, DIFF_FILTER_TEMPERATURE, "FilteredTemperature"));
-      PetscCall(PetscSectionSetFieldName(section, 1, "Filtered Velocity Products"));
+      PetscCall(PetscSectionSetFieldName(section, diff_filter->field_velo_prod, "Filtered Velocity Products"));
       PetscCall(PetscSectionSetComponentName(section, 1, DIFF_FILTER_VELOCITY_SQUARED_XX, "FilteredVelocitySquaredXX"));
       PetscCall(PetscSectionSetComponentName(section, 1, DIFF_FILTER_VELOCITY_SQUARED_YY, "FilteredVelocitySquaredYY"));
       PetscCall(PetscSectionSetComponentName(section, 1, DIFF_FILTER_VELOCITY_SQUARED_ZZ, "FilteredVelocitySquaredZZ"));
