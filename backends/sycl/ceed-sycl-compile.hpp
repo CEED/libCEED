c33a516096b7002b738fc083aa01747716039670
Umesh Unnikrishnan <unnikrishnan@anl.gov>
2023-07-20 15:33:53 +0000

    Fix formatting errors

--- backends/sycl/ceed-sycl-compile.hpp
+++ backends/sycl/ceed-sycl-compile.hpp
@@ -17,7 +17,7 @@
 using SyclModule_t = sycl::kernel_bundle<sycl::bundle_state::executable>;
 
 CEED_INTERN int CeedBuildModule_Sycl(Ceed ceed, const std::string &kernel_source, SyclModule_t **sycl_module,
-                                        const std::map<std::string, CeedInt> &constants = {});
+                                     const std::map<std::string, CeedInt> &constants = {});
 CEED_INTERN int CeedGetKernel_Sycl(Ceed ceed, const SyclModule_t *sycl_module, const std::string &kernel_name, sycl::kernel **sycl_kernel);
 
 CEED_INTERN int CeedRunKernelDimSharedSycl(Ceed ceed, sycl::kernel *kernel, const int grid_size, const int block_size_x, const int block_size_y,
--- include/ceed/jit-source/sycl/sycl-shared-basis-tensor-templates.h
+++ include/ceed/jit-source/sycl/sycl-shared-basis-tensor-templates.h
@@ -176,7 +176,7 @@ inline void ContractTransposeX2d(const CeedInt P_1D, const CeedInt Q_1D, private
 
   scratch[item_id_x + item_id_y * T_1D] = *U;
   work_group_barrier(CLK_LOCAL_MEM_FENCE);
-  
+
   *V = 0.0;
   if (item_id_x < P_1D && item_id_y < P_1D) {
     for (CeedInt i = 0; i < Q_1D; i++) {
