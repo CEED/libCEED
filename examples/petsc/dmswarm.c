9a228689cffbb34650fba619e2a02017b62ccbda
Jeremy L Thompson <jeremy@jeremylt.org>
2023-08-29 06:27:37 -0600

    swarm - add viewing command

--- examples/petsc/dmswarm.c
+++ examples/petsc/dmswarm.c
@@ -179,13 +179,13 @@ int main(int argc, char **argv) {
   }
 
   // -- Set tensor permutation
-  /* {
+  {
     DM dm_coord;
 
     PetscCall(DMGetCoordinateDM(dm_mesh, &dm_coord));
     PetscCall(DMPlexSetClosurePermutationTensor(dm_mesh, PETSC_DETERMINE, NULL));
-    PetscCall(DMPlexSetClosurePermutationTensor(dm_coord, PETSC_DETERMINE, NULL));
-  } */
+    // PetscCall(DMPlexSetClosurePermutationTensor(dm_coord, PETSC_DETERMINE, NULL));
+  }
 
   // -- Final background mesh
   PetscCall(PetscObjectSetName((PetscObject)dm_mesh, "Background Mesh"));
@@ -446,6 +446,9 @@ int main(int argc, char **argv) {
     CeedVectorDestroy(&u_evec);
     CeedVectorDestroy(&u_cell);
   }
+
+  // View result
+  if (!test_mode) PetscCall(DMSwarmViewXDMF(dm_swarm, "swarm.xmf"));
 
   // Cleanup
   CeedDestroy(&ceed);
