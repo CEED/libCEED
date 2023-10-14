129c7fc008c449bba4df41d6bd3a04e25105f6c7
Kenneth E. Jansen <Kenneth.Jansen@colorado.edu>
2023-10-14 12:25:59 -0600

    gdb tested

--- examples/fluids/src/setuplibceed.c
+++ examples/fluids/src/setuplibceed.c
@@ -165,10 +165,8 @@ PetscErrorCode SetupLibceed(Ceed ceed, CeedData ceed_data, DM dm, User user, App
   const PetscInt num_comp_q = 5;
 
   NewtonianIdealGasContext  gas;
-  PetscCallCeed(ceed, CeedQFunctionContextGetDataRead(problem->apply_vol_ifunction.qfunction_context, CEED_MEM_HOST, &gas    ));
-//  CeedInt jac_data_size_vol = num_comp_q + 6 + 3;
-//  jac_data_size_vol += (gas->idl_enable ? 1 : 0);
-  const CeedInt  dim = problem->dim, num_comp_x = problem->dim, q_data_size_vol = problem->q_data_size_vol; jac_data_size_vol = num_comp_q + 6 + 3 + (gas->idl_enable ? 1 : 0);
+  PetscCallCeed(ceed, CeedQFunctionContextGetDataRead(problem->apply_vol_ifunction.qfunction_context, CEED_MEM_HOST, &gas));
+  const CeedInt  dim = problem->dim, num_comp_x = problem->dim, q_data_size_vol = problem->q_data_size_vol, jac_data_size_vol = num_comp_q + 6 + 3 + (gas->idl_enable ? 1 : 0);
   PetscCallCeed(ceed, CeedQFunctionContextRestoreDataRead(problem->apply_vol_ifunction.qfunction_context, &gas));
 
 
