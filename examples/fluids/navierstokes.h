83ef531807566a094c8bb9e52d2c0c9e499463cf
Kenneth E. Jansen <Kenneth.Jansen@colorado.edu>
2023-11-03 14:28:38 -0600

    break out versions

--- examples/fluids/navierstokes.h
+++ examples/fluids/navierstokes.h
@@ -20,7 +20,8 @@
 #error "PETSc v3.20 or later is required"
 #endif
 
-#if PETSC_VERSION_LT(3, 21, 0)
+//#if PETSC_VERSION_GE(3, 21, 0)
+#if ((PETSC_VERSION_MAJOR <4) && (PETSC_VERSION_MINOR < 21))
 #define DMSetCoordinateDisc(a, b, c) DMProjectCoordinates(a, b)
 #endif
 
