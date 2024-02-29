4441641939bedf4429ad966382fec30bf7865c48
Jeremy L Thompson <jeremy@jeremylt.org>
2024-02-29 16:27:10 -0700

    xsmm - add version check for old name, but non-linear versioning in xsmm main

--- backends/xsmm/ceed-xsmm.h
+++ backends/xsmm/ceed-xsmm.h
@@ -11,6 +11,13 @@
 #include <ceed.h>
 #include <ceed/backend.h>
 
+#if ((LIBXSMM_VERSION_MAJOR > 1 || \
+      (LIBXSMM_VERSION_MAJOR == 1 && (LIBXSMM_VERSION_MINOR > 17 || (LIBXSMM_VERSION_MINOR == 17 && LIBXSMM_VERSION_PATCH >= 3710)))))
+#define libxsmm_dispatch_gemm(a, b, c) libxsmm_dispatch_gemm(a, b, c)
+#else
+#define libxsmm_dispatch_gemm(a, b, c) libxsmm_dispatch_gemm_v2(a, b, c)
+#endif
+
 CEED_INTERN int CeedTensorContractCreate_Xsmm(CeedTensorContract contract);
 
 #endif  // CEED_XSMM_H
