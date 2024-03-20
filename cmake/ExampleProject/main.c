40a7b239b752def916364b26756b4e45c1243eff
Sam Mish <samuelpmish@gmail.com>
2024-03-20 10:12:44 -0700

    add example CMake project

---
+++ cmake/ExampleProject/main.c
@@ -0,0 +1,23 @@
+/// @file
+/// Test creation and destruction of a CEED object
+/// \test Test creation and destruction of a CEED object
+#include <ceed.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  Ceed ceed;
+
+  {
+    int major, minor, patch;
+
+    CeedGetVersion(&major, &minor, &patch, NULL);
+    if (!CEED_VERSION_GE(major, minor, patch)) printf("Library version mismatch %d.%d.%d\n", major, minor, patch);
+  }
+
+  CeedInit(argv[1], &ceed);
+  CeedDestroy(&ceed);
+
+  // Test double destroy is safe
+  CeedDestroy(&ceed);
+  return 0;
+}
