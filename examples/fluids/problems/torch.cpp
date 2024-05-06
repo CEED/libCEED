4029f0edadcfe7fd5e951603d9946a9ca335993a
James Wright <james@jameswright.xyz>
2024-05-06 14:01:19 -0600

    Cleanup and some additions

--- examples/fluids/problems/torch.cpp
+++ examples/fluids/problems/torch.cpp
@@ -5,29 +5,57 @@
 #include "../include/libtorch.h"
 
 torch::jit::script::Module model;
-torch::DeviceType          device;
+torch::DeviceType          device_model;
 
-torch::DeviceType Enum2DeviceType(TorchDeviceType device_enum) {
+PetscErrorCode EnumToDeviceType(TorchDeviceType device_enum, torch::DeviceType *device_type) {
+  PetscFunctionBeginUser;
   switch (device_enum) {
     case TORCH_DEVICE_CPU:
-      return torch::kCPU;
+      *device_type = torch::kCPU;
+      break;
     case TORCH_DEVICE_XPU:
-      return torch::kXPU;
+      *device_type = torch::kXPU;
+      break;
     case TORCH_DEVICE_CUDA:
-      return torch::kCUDA;
+      *device_type = torch::kCUDA;
+      break;
     case TORCH_DEVICE_HIP:
-      return torch::kHIP;
+      *device_type = torch::kHIP;
+      break;
+    default:
+      SETERRQ(PETSC_COMM_WORLD, PETSC_ERR_SUP, "TorchDeviceType %d not supported by PyTorch inference", device_enum);
   }
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode PetscMemTypeToDeviceType(PetscMemType mem_type, torch::DeviceType *device_type) {
+  PetscFunctionBeginUser;
+  switch (mem_type) {
+    case PETSC_MEMTYPE_HOST:
+      *device_type = torch::kCPU;
+      break;
+    case PETSC_MEMTYPE_SYCL:
+      *device_type = torch::kXPU;
+      break;
+    case PETSC_MEMTYPE_CUDA:
+      *device_type = torch::kCUDA;
+      break;
+    case PETSC_MEMTYPE_HIP:
+      *device_type = torch::kHIP;
+      break;
+    default:
+      SETERRQ(PETSC_COMM_WORLD, PETSC_ERR_SUP, "PetscMemType %s not supported by PyTorch inference", PetscMemTypeToString(mem_type));
+  }
+  PetscFunctionReturn(PETSC_SUCCESS);
 }
 
 PetscErrorCode LoadModel_LibTorch(const char *model_path, TorchDeviceType device_enum) {
   PetscFunctionBeginUser;
-
-  PetscCallCXX(device = Enum2DeviceType(device_enum));
+  // PetscCallCXX(device_model = EnumToDeviceType(device_enum));
+  PetscCall( EnumToDeviceType(device_enum, &device_model));
 
   PetscCallCXX(model = torch::jit::load(model_path));
-  PetscCallCXX(model.to(torch::Device(device)));
-
+  PetscCallCXX(model.to(torch::Device(device_model)));
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
@@ -36,6 +64,7 @@ PetscErrorCode ModelInference_LibTorch(Vec DD_Inputs_loc, Vec DD_Outputs_loc) {
   torch::Tensor        input_tensor, output_tensor;
   torch::TensorOptions options;
   const PetscInt       num_input_comps = 6, num_output_comps = 6;
+
   PetscFunctionBeginUser;
   // torch::NoGradGuard no_grad; // equivalent to "with torch.no_grad():" in PyTorch
   {
@@ -47,10 +76,11 @@ PetscErrorCode ModelInference_LibTorch(Vec DD_Inputs_loc, Vec DD_Outputs_loc) {
     num_nodes = input_size / num_input_comps;
     PetscCall(VecGetArrayReadAndMemType(DD_Inputs_loc, &dd_inputs_ptr, &input_mem_type));
 
-    PetscCallCXX(options = torch::TensorOptions().dtype(torch::kFloat64).device(device));
-    if (device == torch::kXPU) {  // XPU requires device-to-host-to-device transfer
+    PetscCallCXX(options = torch::TensorOptions().dtype(torch::kFloat64).device(device_model));
+    if (device_model == torch::kXPU) {  // XPU requires device-to-host-to-device transfer
       PetscCallCXX(input_tensor =
-                       at::from_blob((void *)dd_inputs_ptr, {num_nodes, num_input_comps}, {num_input_comps, 1}, nullptr, options, device).to(device));
+                       at::from_blob((void *)dd_inputs_ptr, {num_nodes, num_input_comps}, {num_input_comps, 1}, nullptr, options, device_model)
+                           .to(device_model));
     } else {
       PetscCallCXX(input_tensor = torch::from_blob((void *)dd_inputs_ptr, {num_nodes, num_input_comps}, options));
     }
@@ -60,7 +90,7 @@ PetscErrorCode ModelInference_LibTorch(Vec DD_Inputs_loc, Vec DD_Outputs_loc) {
   // Run model
   PetscCallCXX(output_tensor = model.forward({input_tensor}).toTensor());
 
-  if (device == torch::kXPU) {  // XPU requires device-to-host-to-device transfer
+  if (device_model == torch::kXPU) {  // XPU requires device-to-host-to-device transfer
     PetscInt     output_size;
     PetscScalar *dd_outputs_ptr;
     double      *output_tensor_ptr;
@@ -139,7 +169,7 @@ PetscErrorCode CopyTest(Vec DD_Outputs_loc) {
   PetscCall(VecGetArrayAndMemType(DD_Outputs_loc, &dd_outputs_ptr, &output_mem_type));
 
   auto          dims       = torch::IntArrayRef{output_size};
-  auto          options    = torch::TensorOptions().dtype(torch::kFloat64).device(device);
+  auto          options    = torch::TensorOptions().dtype(torch::kFloat64).device(device_model);
   torch::Tensor gpu_tensor = torch::from_blob((void *)dd_outputs_ptr, dims, options);
 
   torch::Tensor ones = torch::ones_like(gpu_tensor);
