710d288f7218d89616aaf48c6bee48a4b7a123ac
Jeremy L Thompson <jeremy@jeremylt.org>
2024-09-09 14:48:28 -0600

    rust - restore tests from #1451

--- rust/libceed/src/operator.rs
+++ rust/libceed/src/operator.rs
@@ -142,6 +142,14 @@ impl<'a> OperatorField<'a> {
     ///     inputs[0].elem_restriction().is_some(),
     ///     "Incorrect field ElemRestriction"
     /// );
+    /// if let ElemRestrictionOpt::Some(r) = inputs[0].elem_restriction() {
+    ///     assert_eq!(
+    ///         r.num_elements(),
+    ///         ne,
+    ///         "Incorrect field ElemRestriction number of elements"
+    ///     );
+    /// }
+    ///
     /// assert!(
     ///     inputs[1].elem_restriction().is_none(),
     ///     "Incorrect field ElemRestriction"
@@ -153,6 +161,13 @@ impl<'a> OperatorField<'a> {
     ///     outputs[0].elem_restriction().is_some(),
     ///     "Incorrect field ElemRestriction"
     /// );
+    /// if let ElemRestrictionOpt::Some(r) = outputs[0].elem_restriction() {
+    ///     assert_eq!(
+    ///         r.num_elements(),
+    ///         ne,
+    ///         "Incorrect field ElemRestriction number of elements"
+    ///     );
+    /// }
     /// # Ok(())
     /// # }
     /// ```
@@ -196,7 +211,21 @@ impl<'a> OperatorField<'a> {
     /// let inputs = op.inputs()?;
     ///
     /// assert!(inputs[0].basis().is_some(), "Incorrect field Basis");
+    /// if let BasisOpt::Some(b) = inputs[0].basis() {
+    ///     assert_eq!(
+    ///         b.num_quadrature_points(),
+    ///         q,
+    ///         "Incorrect field Basis number of quadrature points"
+    ///     );
+    /// }
     /// assert!(inputs[1].basis().is_some(), "Incorrect field Basis");
+    /// if let BasisOpt::Some(b) = inputs[1].basis() {
+    ///     assert_eq!(
+    ///         b.num_quadrature_points(),
+    ///         q,
+    ///         "Incorrect field Basis number of quadrature points"
+    ///     );
+    /// }
     ///
     /// let outputs = op.outputs()?;
     ///
