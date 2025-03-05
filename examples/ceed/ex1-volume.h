0a242873a29183ba8fb2a56a57a36b831ce52f87
Jeremy L Thompson <jeremy@jeremylt.org>
2025-03-05 11:29:43 -0700

    ex - use VLA casts to clarify qf data shapes

--- examples/ceed/ex1-volume.h
+++ examples/ceed/ex1-volume.h
@@ -14,43 +14,46 @@ struct BuildContext {
 
 /// libCEED Q-function for building quadrature data for a mass operator
 CEED_QFUNCTION(build_mass)(void *ctx, const CeedInt Q, const CeedScalar *const *in, CeedScalar *const *out) {
-  // in[0] is Jacobians with shape [dim, nc=dim, Q]
-  // in[1] is quadrature weights, size (Q)
+  // in[0] is Jacobians with shape [dim, dim, Q]
+  // in[1] is quadrature weights with shape [1, Q]
+  const CeedScalar    *w          = in[1];
+  CeedScalar          *q_data     = out[0];
   struct BuildContext *build_data = (struct BuildContext *)ctx;
-  const CeedScalar    *J = in[0], *w = in[1];
-  CeedScalar          *q_data = out[0];
 
   switch (build_data->dim + 10 * build_data->space_dim) {
-    case 11:
+    case 11: {
+      const CeedScalar(*J)[1][CEED_Q_VLA] = (const CeedScalar(*)[1][CEED_Q_VLA])in[0];
+
       // Quadrature Point Loop
-      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) { q_data[i] = J[i] * w[i]; }  // End of Quadrature Point Loop
-      break;
-    case 22:
-      // Quadrature Point Loop
-      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) {
-        // 0 2
-        // 1 3
-        q_data[i] = (J[i + Q * 0] * J[i + Q * 3] - J[i + Q * 1] * J[i + Q * 2]) * w[i];
-      }  // End of Quadrature Point Loop
-      break;
-    case 33:
+      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) { q_data[i] = J[0][0][i] * w[i]; }  // End of Quadrature Point Loop
+    } break;
+    case 22: {
+      const CeedScalar(*J)[2][CEED_Q_VLA] = (const CeedScalar(*)[2][CEED_Q_VLA])in[0];
+
       // Quadrature Point Loop
       CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) {
-        // 0 3 6
-        // 1 4 7
-        // 2 5 8
-        q_data[i] = (J[i + Q * 0] * (J[i + Q * 4] * J[i + Q * 8] - J[i + Q * 5] * J[i + Q * 7]) -
-                     J[i + Q * 1] * (J[i + Q * 3] * J[i + Q * 8] - J[i + Q * 5] * J[i + Q * 6]) +
-                     J[i + Q * 2] * (J[i + Q * 3] * J[i + Q * 7] - J[i + Q * 4] * J[i + Q * 6])) *
-                    w[i];
+        q_data[i] = (J[0][0][i] * J[1][1][i] - J[0][1][i] * J[1][0][i]) * w[i];
       }  // End of Quadrature Point Loop
-      break;
+    } break;
+    case 33: {
+      const CeedScalar(*J)[3][CEED_Q_VLA] = (const CeedScalar(*)[3][CEED_Q_VLA])in[0];
+
+      // Quadrature Point Loop
+      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) {
+        q_data[i] =
+            (J[0][0][i] * (J[1][1][i] * J[2][2][i] - J[1][2][i] * J[2][1][i]) - J[0][1][i] * (J[1][0][i] * J[2][2][i] - J[1][2][i] * J[2][0][i]) +
+             J[0][2][i] * (J[1][0][i] * J[2][1][i] - J[1][1][i] * J[2][0][i])) *
+            w[i];
+      }  // End of Quadrature Point Loop
+    } break;
   }
   return CEED_ERROR_SUCCESS;
 }
 
 /// libCEED Q-function for applying a mass operator
 CEED_QFUNCTION(apply_mass)(void *ctx, const CeedInt Q, const CeedScalar *const *in, CeedScalar *const *out) {
+  // in[0], out[0] are solution variables with shape [1, Q]
+  // in[1] is quadrature data with shape [1, Q]
   const CeedScalar *u = in[0], *q_data = in[1];
   CeedScalar       *v = out[0];
 
--- examples/ceed/ex2-surface.h
+++ examples/ceed/ex2-surface.h
@@ -14,63 +14,69 @@ struct BuildContext {
 
 /// libCEED Q-function for building quadrature data for a diffusion operator
 CEED_QFUNCTION(build_diff)(void *ctx, const CeedInt Q, const CeedScalar *const *in, CeedScalar *const *out) {
-  struct BuildContext *build_data = (struct BuildContext *)ctx;
-  // in[0] is Jacobians with shape [dim, nc=dim, Q]
+  // in[0] is Jacobians with shape [dim, dim, Q]
   // in[1] is quadrature weights, size (Q)
-  //
+  const CeedScalar *w             = in[1];
+  CeedScalar(*q_data)[CEED_Q_VLA] = (CeedScalar(*)[CEED_Q_VLA])out[0];
+  struct BuildContext *build_data = (struct BuildContext *)ctx;
+
   // At every quadrature point, compute w/det(J).adj(J).adj(J)^T and store
   // the symmetric part of the result.
-  const CeedScalar *J = in[0], *w = in[1];
-  CeedScalar       *q_data = out[0];
-
   switch (build_data->dim + 10 * build_data->space_dim) {
-    case 11:
-      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) { q_data[i] = w[i] / J[i]; }  // End of Quadrature Point Loop
-      break;
-    case 22:
-      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) {
-        // J: 0 2   q_data: 0 2   adj(J):  J22 -J12
-        //    1 3           2 1           -J21  J11
-        const CeedScalar J11 = J[i + Q * 0];
-        const CeedScalar J21 = J[i + Q * 1];
-        const CeedScalar J12 = J[i + Q * 2];
-        const CeedScalar J22 = J[i + Q * 3];
-        const CeedScalar qw  = w[i] / (J11 * J22 - J21 * J12);
+    case 11: {
+      const CeedScalar(*J)[1][CEED_Q_VLA] = (const CeedScalar(*)[1][CEED_Q_VLA])in[0];
 
-        q_data[i + Q * 0] = qw * (J12 * J12 + J22 * J22);
-        q_data[i + Q * 1] = qw * (J11 * J11 + J21 * J21);
-        q_data[i + Q * 2] = -qw * (J11 * J12 + J21 * J22);
+      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) { q_data[0][i] = w[i] / J[0][0][i]; }  // End of Quadrature Point Loop
+    } break;
+    case 22: {
+      const CeedScalar(*J)[2][CEED_Q_VLA] = (const CeedScalar(*)[2][CEED_Q_VLA])in[0];
+
+      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) {
+        // J: 0 2   q_data: 0 2   adj(J):  J11 -J01
+        //    1 3           2 1           -J10  J00
+        const CeedScalar J00 = J[0][0][i];
+        const CeedScalar J10 = J[0][1][i];
+        const CeedScalar J01 = J[1][0][i];
+        const CeedScalar J11 = J[1][1][i];
+        const CeedScalar qw  = w[i] / (J00 * J11 - J10 * J01);
+
+        q_data[0][i] = qw * (J01 * J01 + J11 * J11);
+        q_data[1][i] = qw * (J00 * J00 + J10 * J10);
+        q_data[2][i] = -qw * (J00 * J01 + J10 * J11);
       }  // End of Quadrature Point Loop
-      break;
-    case 33:
+    } break;
+    case 33: {
+      const CeedScalar(*J)[3][CEED_Q_VLA] = (const CeedScalar(*)[3][CEED_Q_VLA])in[0];
+
       CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) {
         // Compute the adjoint
         CeedScalar A[3][3];
+
         for (CeedInt j = 0; j < 3; j++) {
           for (CeedInt k = 0; k < 3; k++) {
             // Equivalent code with J as a VLA and no mod operations:
             // A[k][j] = J[j+1][k+1]*J[j+2][k+2] - J[j+1][k+2]*J[j+2][k+1]
-            A[k][j] = J[i + Q * ((j + 1) % 3 + 3 * ((k + 1) % 3))] * J[i + Q * ((j + 2) % 3 + 3 * ((k + 2) % 3))] -
-                      J[i + Q * ((j + 1) % 3 + 3 * ((k + 2) % 3))] * J[i + Q * ((j + 2) % 3 + 3 * ((k + 1) % 3))];
+            A[k][j] =
+                J[(k + 1) % 3][(j + 1) % 3][i] * J[(k + 2) % 3][(j + 2) % 3][i] - J[(k + 2) % 3][(j + 1) % 3][i] * J[(k + 1) % 3][(j + 2) % 3][i];
           }
         }
 
         // Compute quadrature weight / det(J)
-        const CeedScalar qw = w[i] / (J[i + Q * 0] * A[0][0] + J[i + Q * 1] * A[0][1] + J[i + Q * 2] * A[0][2]);
+        const CeedScalar qw = w[i] / (J[0][0][i] * A[0][0] + J[0][1][i] * A[0][1] + J[0][2][i] * A[0][2]);
 
         // Compute geometric factors
         // Stored in Voigt convention
         // 0 5 4
         // 5 1 3
         // 4 3 2
-        q_data[i + Q * 0] = qw * (A[0][0] * A[0][0] + A[0][1] * A[0][1] + A[0][2] * A[0][2]);
-        q_data[i + Q * 1] = qw * (A[1][0] * A[1][0] + A[1][1] * A[1][1] + A[1][2] * A[1][2]);
-        q_data[i + Q * 2] = qw * (A[2][0] * A[2][0] + A[2][1] * A[2][1] + A[2][2] * A[2][2]);
-        q_data[i + Q * 3] = qw * (A[1][0] * A[2][0] + A[1][1] * A[2][1] + A[1][2] * A[2][2]);
-        q_data[i + Q * 4] = qw * (A[0][0] * A[2][0] + A[0][1] * A[2][1] + A[0][2] * A[2][2]);
-        q_data[i + Q * 5] = qw * (A[0][0] * A[1][0] + A[0][1] * A[1][1] + A[0][2] * A[1][2]);
+        q_data[0][i] = qw * (A[0][0] * A[0][0] + A[0][1] * A[0][1] + A[0][2] * A[0][2]);
+        q_data[1][i] = qw * (A[1][0] * A[1][0] + A[1][1] * A[1][1] + A[1][2] * A[1][2]);
+        q_data[2][i] = qw * (A[2][0] * A[2][0] + A[2][1] * A[2][1] + A[2][2] * A[2][2]);
+        q_data[3][i] = qw * (A[1][0] * A[2][0] + A[1][1] * A[2][1] + A[1][2] * A[2][2]);
+        q_data[4][i] = qw * (A[0][0] * A[2][0] + A[0][1] * A[2][1] + A[0][2] * A[2][2]);
+        q_data[5][i] = qw * (A[0][0] * A[1][0] + A[0][1] * A[1][1] + A[0][2] * A[1][2]);
       }  // End of Quadrature Point Loop
-      break;
+    } break;
   }
   return CEED_ERROR_SUCCESS;
 }
@@ -78,50 +84,55 @@ CEED_QFUNCTION(build_diff)(void *ctx, const CeedInt Q, const CeedScalar *const *
 /// libCEED Q-function for applying a diff operator
 CEED_QFUNCTION(apply_diff)(void *ctx, const CeedInt Q, const CeedScalar *const *in, CeedScalar *const *out) {
   struct BuildContext *build_data = (struct BuildContext *)ctx;
-  // in[0], out[0] have shape [dim, nc=1, Q]
-  const CeedScalar *ug = in[0], *q_data = in[1];
-  CeedScalar       *vg = out[0];
+  // in[0], out[0] solution gradients with shape [dim, 1, Q]
+  // in[1] is quadrature data with shape [num_components, Q]
+  const CeedScalar(*q_data)[CEED_Q_VLA] = (const CeedScalar(*)[CEED_Q_VLA])in[1];
 
   switch (build_data->dim) {
-    case 1:
-      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) { vg[i] = ug[i] * q_data[i]; }  // End of Quadrature Point Loop
-      break;
-    case 2:
-      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) {
-        // Read spatial derivatives of u
-        const CeedScalar du[2] = {ug[i + Q * 0], ug[i + Q * 1]};
+    case 1: {
+      const CeedScalar *ug = in[0];
+      CeedScalar       *vg = out[0];
 
+      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) { vg[i] = ug[i] * q_data[0][i]; }  // End of Quadrature Point Loop
+    } break;
+    case 2: {
+      const CeedScalar(*ug)[CEED_Q_VLA] = (const CeedScalar(*)[CEED_Q_VLA])in[0];
+      CeedScalar(*vg)[CEED_Q_VLA]       = (CeedScalar(*)[CEED_Q_VLA])out[0];
+
+      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) {
         // Read q_data (dXdxdXdx_T symmetric matrix)
         // Stored in Voigt convention
         // 0 2
         // 2 1
         const CeedScalar dXdxdXdx_T[2][2] = {
-            {q_data[i + 0 * Q], q_data[i + 2 * Q]},
-            {q_data[i + 2 * Q], q_data[i + 1 * Q]}
+            {q_data[0][i], q_data[2][i]},
+            {q_data[2][i], q_data[1][i]}
         };
-        // j = direction of vg
-        for (int j = 0; j < 2; j++) vg[i + j * Q] = (du[0] * dXdxdXdx_T[0][j] + du[1] * dXdxdXdx_T[1][j]);
-      }  // End of Quadrature Point Loop
-      break;
-    case 3:
-      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) {
-        // Read spatial derivatives of u
-        const CeedScalar du[3] = {ug[i + Q * 0], ug[i + Q * 1], ug[i + Q * 2]};
 
+        // j = direction of vg
+        for (int j = 0; j < 2; j++) vg[j][i] = (ug[0][i] * dXdxdXdx_T[0][j] + ug[1][i] * dXdxdXdx_T[1][j]);
+      }  // End of Quadrature Point Loop
+    } break;
+    case 3: {
+      const CeedScalar(*ug)[CEED_Q_VLA] = (const CeedScalar(*)[CEED_Q_VLA])in[0];
+      CeedScalar(*vg)[CEED_Q_VLA]       = (CeedScalar(*)[CEED_Q_VLA])out[0];
+
+      CeedPragmaSIMD for (CeedInt i = 0; i < Q; i++) {
         // Read q_data (dXdxdXdx_T symmetric matrix)
         // Stored in Voigt convention
         // 0 5 4
         // 5 1 3
         // 4 3 2
         const CeedScalar dXdxdXdx_T[3][3] = {
-            {q_data[i + 0 * Q], q_data[i + 5 * Q], q_data[i + 4 * Q]},
-            {q_data[i + 5 * Q], q_data[i + 1 * Q], q_data[i + 3 * Q]},
-            {q_data[i + 4 * Q], q_data[i + 3 * Q], q_data[i + 2 * Q]}
+            {q_data[0][i], q_data[5][i], q_data[4][i]},
+            {q_data[5][i], q_data[1][i], q_data[3][i]},
+            {q_data[4][i], q_data[3][i], q_data[2][i]}
         };
+
         // j = direction of vg
-        for (int j = 0; j < 3; j++) vg[i + j * Q] = (du[0] * dXdxdXdx_T[0][j] + du[1] * dXdxdXdx_T[1][j] + du[2] * dXdxdXdx_T[2][j]);
+        for (int j = 0; j < 3; j++) vg[j][i] = (ug[0][i] * dXdxdXdx_T[0][j] + ug[1][i] * dXdxdXdx_T[1][j] + ug[2][i] * dXdxdXdx_T[2][j]);
       }  // End of Quadrature Point Loop
-      break;
+    } break;
   }
   return CEED_ERROR_SUCCESS;
 }
