e884ba5ed9faae54c134e02342daf1bfd1661c6e
Jeremy L Thompson <jeremy@jeremylt.org>
2025-03-14 15:00:45 -0600

    fix - harmless warnings

--- backends/hip-gen/ceed-hip-gen-operator-build.cpp
+++ backends/hip-gen/ceed-hip-gen-operator-build.cpp
@@ -1081,7 +1081,7 @@ static int CeedOperatorBuildKernelQFunction_Hip_gen(std::ostringstream &code, Ce
 extern "C" int CeedOperatorBuildKernel_Hip_gen(CeedOperator op, bool *is_good_build) {
   bool                   is_all_tensor = true, is_all_nontensor = true, is_at_points = false, use_3d_slices = false;
   Ceed                   ceed;
-  CeedInt                Q, Q_1d, num_input_fields, num_output_fields, max_dim = 1, max_num_points = 0, coords_comp_stride = 0;
+  CeedInt                Q = 0, Q_1d = 0, num_input_fields, num_output_fields, max_dim = 1, max_num_points = 0, coords_comp_stride = 0;
   CeedQFunctionField    *qf_input_fields, *qf_output_fields;
   CeedQFunction_Hip_gen *qf_data;
   CeedQFunction          qf;
@@ -1165,7 +1165,7 @@ extern "C" int CeedOperatorBuildKernel_Hip_gen(CeedOperator op, bool *is_good_bu
   // Get operator data
   CeedCallBackend(CeedOperatorIsAtPoints(op, &is_at_points));
   {
-    CeedInt max_P, max_P_1d;
+    CeedInt max_P = 0, max_P_1d = 0;
 
     CeedCallBackend(CeedOperatorBuildKernelData_Hip_gen(ceed, num_input_fields, op_input_fields, qf_input_fields, num_output_fields, op_output_fields,
                                                         qf_output_fields, &max_P, &max_P_1d, &Q, &Q_1d, &max_dim, &is_all_tensor, &use_3d_slices));
