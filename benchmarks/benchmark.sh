d275d636ccaa61e594421fac80252590e7a77ccf
Jeremy L Thompson <jeremy@jeremylt.org>
2025-03-19 16:27:57 -0600

    minor - upate copyright to 2025

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2017-2024, Lawrence Livermore National Security, LLC and other CEED contributors.
+# Copyright (c) 2017-2025, Lawrence Livermore National Security, LLC and other CEED contributors.
 # All Rights Reserved. See the top-level LICENSE and NOTICE files for details.
 #
 # SPDX-License-Identifier: BSD-2-Clause
