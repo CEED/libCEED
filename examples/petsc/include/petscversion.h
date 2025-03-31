bfbde1a268fe98bfe2ba1c9d50ebdf542389ce5e
Jeremy L Thompson <jeremy@jeremylt.org>
2025-03-31 10:48:08 -0600

    petsc - require 3.23

--- examples/petsc/include/petscversion.h
+++ examples/petsc/include/petscversion.h
@@ -9,6 +9,6 @@
 /// Petsc version check
 #pragma once
 
-#if PETSC_VERSION_LT(3, 22, 0)
-#error "PETSc v3.22 or later is required"
+#if PETSC_VERSION_LT(3, 23, 0)
+#error "PETSc v3.23 or later is required"
 #endif
