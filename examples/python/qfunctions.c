e12e925e7c624c25d201a1eca0bf7046d0759520
Arshia Ilaty <58371460+ArshiaIlaty@users.noreply.github.com>
2025-05-02 11:00:48 -0700

    Update qfunctions.c to address the comment

--- examples/python/qfunctions.c
+++ examples/python/qfunctions.c
@@ -1,5 +1,11 @@
-#include <ceed.h>
+// Copyright (c) 2017-2025, Lawrence Livermore National Security, LLC and other CEED contributors.
+// All Rights Reserved. See the top-level LICENSE and NOTICE files for details.
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This file is part of CEED:  http://github.com/ceed
 
+#include <ceed.h>
 // -----------------------------------------------------------------------------
 // Redefine QFunction Macro
 // -----------------------------------------------------------------------------
@@ -11,21 +17,4 @@
 // -----------------------------------------------------------------------------
 #include "ex3-volume.h"
 
-// Export the wrapper functions
-#ifdef _WIN32
-#define EXPORT __declspec(dllexport)
-#else
-#define EXPORT __attribute__((visibility("default")))
-#endif
-
-EXPORT CeedInt wrap_build_mass_diff(void *ctx, const CeedInt Q,
-                                  const CeedScalar *const *in,
-                                  CeedScalar *const *out) {
-  return build_mass_diff(ctx, Q, in, out);
-}
-
-EXPORT CeedInt wrap_apply_mass_diff(void *ctx, const CeedInt Q,
-                                  const CeedScalar *const *in,
-                                  CeedScalar *const *out) {
-  return apply_mass_diff(ctx, Q, in, out);
-}
+// -----------------------------------------------------------------------------
