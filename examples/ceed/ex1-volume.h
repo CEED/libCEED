b71448f658e79682681c67fcabd6483f72e96cf7
Allen MacFarland <alma4974@noether>
2025-07-01 11:47:51 -0600

    Update ex1-volume.h

--- examples/ceed/ex1-volume.h
+++ examples/ceed/ex1-volume.h
@@ -12,9 +12,9 @@ struct BuildContext {
   CeedInt dim, space_dim;
 };
 
-//#pragma comment(lib, "libbruh.a")
 
-extern "C" __device__ uint32_t add_num(uint32_t x);
+
+extern uint32_t add_num(uint32_t x);
 
 /// libCEED Q-function for building quadrature data for a mass operator
 CEED_QFUNCTION(build_mass)(void *ctx, const CeedInt Q, const CeedScalar *const *in, CeedScalar *const *out) {
@@ -24,8 +24,8 @@ CEED_QFUNCTION(build_mass)(void *ctx, const CeedInt Q, const CeedScalar *const *
   CeedScalar          *q_data     = out[0];
   struct BuildContext *build_data = (struct BuildContext *)ctx;
 
-
-  volatile uint32_t var = add_num(3);
+  uint32_t num = 3;
+  volatile uint32_t var = add_num(num);
 
   switch (build_data->dim + 10 * build_data->space_dim) {
     case 11: {
