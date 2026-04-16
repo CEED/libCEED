5ad8d86d570c99aa4e993d268e5ac6effb8d67d1
Jeremy L Thompson <jeremy@jeremylt.org>
2026-04-16 10:22:30 -0600

    hip - don't use JiT options that chipStar cannot process

---
+++ include/ceed/jit-source/hip/hip-chipstar.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2017-2026, Lawrence Livermore National Security, LLC and other CEED contributors.
+// All Rights Reserved. See the top-level LICENSE and NOTICE files for details.
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This file is part of CEED:  http://github.com/ceed
+
+/// @file
+/// Internal header for HIP chipStar detection
+
+// If we are using Chipstar, then we have to ensure all threads have the same workloads
+//   and hit __syncthreads() at the same places/number of times
+#ifdef __HIP_PLATFORM_SPIRV__
+#define CEED_HIP_USE_CHIPSTAR true
+#else
+#define CEED_HIP_USE_CHIPSTAR false
+#endif
