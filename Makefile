libxsmm/libxsmm

commit ca8c7b2c4fa4ee01bbb775a725a9bcca61fb3d34

Install include files into subdir (include/libxsmm) (#1077)
Co-authored-by: Alexander Heinecke <alexander.heinecke@intel.com>

Makefile
@@ -17,7 +17,7 @@ UTLDIR := $(SPLDIR)/utilities
 DOCDIR := documentation
 
 # subdirectories (relative) to PREFIX (install targets)
-PINCDIR ?= $(INCDIR)
+PINCDIR ?= $(INCDIR)/libxsmm
 PSRCDIR ?= libxsmm
 POUTDIR ?= $(OUTDIR)
 PPKGDIR ?= $(OUTDIR)/pkgconfig
@@ -1361,7 +1361,7 @@ $(OUTDIR)/libxsmm.env: $(OUTDIR)/.make $(INCDIR)/libxsmm.h
 	@echo >>$@
 	@echo "prepend-path PKG_CONFIG_PATH \"\$$PREFIX/$(PPKGDIR)\"" >>$@
 	@echo "prepend-path LIBRARY_PATH \"\$$PREFIX/lib\"" >>$@
-	@echo "prepend-path CPATH \"\$$PREFIX/include\"" >>$@
+	@echo "prepend-path CPATH \"\$$PREFIX/$(PINCDIR)\"" >>$@
 
 .PHONY: deb
 deb:

.github/workflows/cmake.yml
@@ -53,11 +53,11 @@ jobs:
 
     - name: Verify install
       run: |
-        test -f $HOME/libxsmm-install/include/libxsmm.h
-        test -f $HOME/libxsmm-install/include/libxsmm_config.h
-        test -f $HOME/libxsmm-install/include/libxsmm_source.h
-        test -f $HOME/libxsmm-install/include/libxsmm_version.h
-        test -d $HOME/libxsmm-install/include/utils
+        test -f $HOME/libxsmm-install/include/libxsmm/libxsmm.h
+        test -f $HOME/libxsmm-install/include/libxsmm/libxsmm_config.h
+        test -f $HOME/libxsmm-install/include/libxsmm/libxsmm_source.h
+        test -f $HOME/libxsmm-install/include/libxsmm/libxsmm_version.h
+        test -d $HOME/libxsmm-install/include/libxsmm/utils
         ls $HOME/libxsmm-install/lib/libxsmm* >/dev/null
         ls $HOME/libxsmm-install/lib/pkgconfig/libxsmm.pc >/dev/null
         test -f $HOME/libxsmm-install/lib/cmake/libxsmm/libxsmmConfig.cmake

CMakeLists.txt
@@ -29,6 +29,8 @@ option(BUILD_SHARED_LIBS "Build libxsmm as a shared library" OFF)
 option(BUILD_TESTING "Build unittests" OFF)
 option(LIBXSMM_FORTRAN "Build Fortran module interface (auto-detected if not set)" OFF)
 
+set(LIBXSMM_INSTALL_INCLUDEDIR "${CMAKE_INSTALL_INCLUDEDIR}/libxsmm" CACHE STRING
+  "Installation directory for LIBXSMM headers")
 set(LIBXSMM_INSTALL_PKGCONFIGDIR "${CMAKE_INSTALL_LIBDIR}/pkgconfig" CACHE STRING
   "Installation directory for pkg-config files")
 
@@ -200,7 +202,7 @@ target_include_directories(xsmm
   PUBLIC
     $<BUILD_INTERFACE:${XSMM_INCLUDE_DIRS}>
     $<BUILD_INTERFACE:${XSMM_GENERATED_INCLUDE_DIR}>
-    $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+    $<INSTALL_INTERFACE:${LIBXSMM_INSTALL_INCLUDEDIR}>)
 
 target_compile_definitions(xsmm
   PUBLIC
@@ -284,7 +286,7 @@ if(LIBXSMM_FORTRAN)
   target_include_directories(xsmmf
     PUBLIC
       $<BUILD_INTERFACE:${XSMM_Fortran_MODULE_DIR}>
-      $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+      $<INSTALL_INTERFACE:${LIBXSMM_INSTALL_INCLUDEDIR}>)
 
   list(APPEND LIBXSMM_INSTALL_TARGETS xsmmf)
 endif()
@@ -321,19 +323,19 @@ install(TARGETS ${LIBXSMM_INSTALL_TARGETS}
   RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
 
 install(DIRECTORY ${XSMM_ROOT_DIR}/include/
-  DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+  DESTINATION ${LIBXSMM_INSTALL_INCLUDEDIR}
   FILES_MATCHING
     PATTERN "*.h"
     PATTERN "*.hpp"
     PATTERN "*.f")
 install(FILES ${XSMM_GENERATED_INCLUDE_DIR}/libxsmm_version.h
-  DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+  DESTINATION ${LIBXSMM_INSTALL_INCLUDEDIR})
 if(LIBXSMM_FORTRAN)
   install(FILES ${XSMM_GENERATED_FORTRAN_SOURCE}
-    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+    DESTINATION ${LIBXSMM_INSTALL_INCLUDEDIR}
     RENAME libxsmm.f)
   install(DIRECTORY ${XSMM_Fortran_MODULE_DIR}/
-    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+    DESTINATION ${LIBXSMM_INSTALL_INCLUDEDIR}
     FILES_MATCHING PATTERN "*.mod")
 endif()
 install(EXPORT libxsmmTargets

scripts/libxsmm.pc.in
@@ -1,6 +1,6 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}
-includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@
+includedir=${prefix}/@LIBXSMM_INSTALL_INCLUDEDIR@
 libdir=${prefix}/@CMAKE_INSTALL_LIBDIR@
 
 Name: libxsmm

scripts/libxsmmConfig.cmake
@@ -18,10 +18,16 @@ set(CMAKE_FIND_LIBRARY_SUFFIXES "${_libxsmm_suffixes_save}")
 unset(_libxsmm_suffixes_save)
 
 find_path(LIBXSMM_INCLUDE_DIR NAMES libxsmm.h
-  HINTS "${_libxsmm_prefix}/include" "${_libxsmm_prefix}" NO_DEFAULT_PATH)
+  HINTS "${_libxsmm_prefix}/include/libxsmm"
+        "${_libxsmm_prefix}/include"
+        "${_libxsmm_prefix}"
+  NO_DEFAULT_PATH)
 
 find_path(LIBXSMM_FORTRAN_MODULE_DIR NAMES libxsmm.mod LIBXSMM.mod
-  HINTS "${_libxsmm_prefix}/include" "${_libxsmm_prefix}" NO_DEFAULT_PATH)
+  HINTS "${_libxsmm_prefix}/include/libxsmm"
+        "${_libxsmm_prefix}/include"
+        "${_libxsmm_prefix}"
+  NO_DEFAULT_PATH)
 
 set(_libxsmm_suffixes_save "${CMAKE_FIND_LIBRARY_SUFFIXES}")
 if(DEFINED BUILD_SHARED_LIBS AND NOT BUILD_SHARED_LIBS)

scripts/libxsmmf.pc.in
@@ -1,6 +1,6 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}
-includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@
+includedir=${prefix}/@LIBXSMM_INSTALL_INCLUDEDIR@
 libdir=${prefix}/@CMAKE_INSTALL_LIBDIR@
 
 Name: libxsmm/f

CEED/libCEED

commit cbe3022cc72ae3ba727e8c0d26c63388dff79616

make - fix bug introduced in https://github.com/libxsmm/libxsmm/pull/1077

Makefile
@@ -562,7 +562,7 @@ ifneq ($(wildcard $(XSMM_DIR)/lib/libxsmm.*),)
   PKG_LIBS += $(BLAS_LIB)
   libceed.c += $(xsmm.c)
   libceed.h += $(xsmm.h)
-  $(xsmm.c:%.c=$(OBJDIR)/%.o) $(xsmm.c:%=%.tidy) $(xsmm.c:%=%.tidy-fix) $(xsmm.h:%=%.tidy-fix): CPPFLAGS += -I$(XSMM_DIR)/include
+  $(xsmm.c:%.c=$(OBJDIR)/%.o) $(xsmm.c:%=%.tidy) $(xsmm.c:%=%.tidy-fix) $(xsmm.h:%=%.tidy-fix): CPPFLAGS += -I$(XSMM_DIR)/include -I$(XSMM_DIR)/include/libxsmm
   BACKENDS_MAKE += $(XSMM_BACKENDS)
 endif
 

examples/petsc/Makefile
@@ -12,7 +12,7 @@ COMMON ?= ../../common.mk
 #       PETSC_ARCH - for example when using PETSc installed through Spack.
 PETSc.pc := $(PETSC_DIR)/$(PETSC_ARCH)/lib/pkgconfig/PETSc.pc
 CEED_DIR ?= ../..
-ceed.pc := $(CEED_DIR)/lib/pkgconfig/ceed.pc
+ceed.pc  := $(CEED_DIR)/lib/pkgconfig/ceed.pc
 
 CC = $(call pkgconf, --variable=ccompiler $(PETSc.pc) $(ceed.pc))
 CFLAGS = -std=c11 \
@@ -21,9 +21,9 @@ CFLAGS = -std=c11 \
   $(OPT)
 CPPFLAGS = $(call pkgconf, --cflags-only-I $(PETSc.pc) $(ceed.pc)) \
   $(call pkgconf, --variable=cflags_dep $(PETSc.pc))
-LDFLAGS = $(call pkgconf, --libs-only-L --libs-only-other $(PETSc.pc) $(ceed.pc))
+LDFLAGS  = $(call pkgconf, --libs-only-L --libs-only-other $(PETSc.pc) $(ceed.pc))
 LDFLAGS += $(patsubst -L%, $(call pkgconf, --variable=ldflag_rpath $(PETSc.pc))%, $(call pkgconf, --libs-only-L $(PETSc.pc) $(ceed.pc)))
-LDLIBS = $(call pkgconf, --libs-only-l $(PETSc.pc) $(ceed.pc)) -lm
+LDLIBS   = $(call pkgconf, --libs-only-l $(PETSc.pc) $(ceed.pc)) -lm
 
 OBJDIR := build
 SRCDIR := src