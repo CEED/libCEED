d7ed0dad043954305ab45e7b1c3a706015066556
Zach Atkins <Zach.Atkins@colorado.edu>
2026-09-11 17:48:54 -0600

    cpu(gen) - fix incorrect ordering in tensor grad basis

--- include/ceed/jit-source/cpu-gen/cpu-gen-basis-templates.h
+++ include/ceed/jit-source/cpu-gen/cpu-gen-basis-templates.h
@@ -36,7 +36,7 @@ template <CeedInt A, CeedInt B, CeedInt C, CeedInt J>
 static inline int TensorContract_Apply_NoTranspose(const CeedScalar *t, const CeedScalar *u, CeedScalar *v) {
   for (CeedInt q = 0; q < A * J * C; q++) v[q] = (CeedScalar)0.0;
 
-  CeedCall(TensorContract_ApplyAdd_NoTranspose<A, B, C, J>(t, u, v));
+  TensorContract_ApplyAdd_NoTranspose<A, B, C, J>(t, u, v);
   return CEED_ERROR_SUCCESS;
 }
 
@@ -60,7 +60,7 @@ template <CeedInt A, CeedInt B, CeedInt C, CeedInt J>
 static inline int TensorContract_Apply_Transpose(const CeedScalar *t, const CeedScalar *u, CeedScalar *v) {
   for (CeedInt q = 0; q < A * J * C; q++) v[q] = (CeedScalar)0.0;
 
-  CeedCall(TensorContract_ApplyAdd_Transpose<A, B, C, J>(t, u, v));
+  TensorContract_ApplyAdd_Transpose<A, B, C, J>(t, u, v);
   return CEED_ERROR_SUCCESS;
 }
 
@@ -71,16 +71,16 @@ static inline int TensorContract_Apply_Transpose(const CeedScalar *t, const Ceed
 template <CeedInt BLOCK_SIZE, CeedInt NUM_COMP, CeedInt Q_COMP, CeedInt P, CeedInt Q>
 static inline int CeedBasis_Apply_NoTranspose_NonTensor(const CeedScalar *mat, const CeedScalar *u, CeedScalar *v) {
   for (CeedInt d = 0; d < Q_COMP; d++) {
-    CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP, P, BLOCK_SIZE, Q>(mat + d * P * Q, u, v + d * NUM_COMP * BLOCK_SIZE * Q));
+    TensorContract_Apply_NoTranspose<NUM_COMP, P, BLOCK_SIZE, Q>(mat + d * P * Q, u, v + d * NUM_COMP * BLOCK_SIZE * Q);
   }
   return CEED_ERROR_SUCCESS;
 }
 
 template <CeedInt BLOCK_SIZE, CeedInt NUM_COMP, CeedInt Q_COMP, CeedInt P, CeedInt Q>
 static inline int CeedBasis_Apply_Transpose_NonTensor(const CeedScalar *mat, const CeedScalar *u, CeedScalar *v) {
-  CeedCall(TensorContract_Apply_Transpose<NUM_COMP, Q, BLOCK_SIZE, P>(mat, u, v));
-  for (CeedInt d = 1; d < Q_COMP; d++) {
-    CeedCall(TensorContract_ApplyAdd_Transpose<NUM_COMP, Q, BLOCK_SIZE, P>(mat + d * P * Q, u + d * NUM_COMP * BLOCK_SIZE * Q, v));
+  for (CeedInt q = 0; q < P * NUM_COMP * BLOCK_SIZE; q++) v[q] = (CeedScalar)0.0;
+  for (CeedInt d = 0; d < Q_COMP; d++) {
+    TensorContract_ApplyAdd_Transpose<NUM_COMP, Q, BLOCK_SIZE, P>(mat + d * P * Q, u + d * NUM_COMP * BLOCK_SIZE * Q, v);
   }
   return CEED_ERROR_SUCCESS;
 }
@@ -219,11 +219,11 @@ static inline int CeedBasis_Apply_NoTranspose_Grad_Tensor_2D(const CeedScalar *i
                                                              CeedScalar *v) {
   CeedScalar temp[BLOCK_SIZE * NUM_COMP * Q_1D * CeedIntMax(Q_1D, P_1D)];
 
-  CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP * P_1D, P_1D, BLOCK_SIZE, Q_1D>(interp_1d, u, temp));
-  CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP, P_1D, BLOCK_SIZE * Q_1D, Q_1D>(grad_1d, temp, v));
-
   CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP * P_1D, P_1D, BLOCK_SIZE, Q_1D>(grad_1d, u, temp));
-  CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP, P_1D, BLOCK_SIZE * Q_1D, Q_1D>(interp_1d, temp, &v[BLOCK_SIZE * NUM_COMP * Q_1D * Q_1D]));
+  CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP, P_1D, BLOCK_SIZE * Q_1D, Q_1D>(interp_1d, temp, v));
+
+  CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP * P_1D, P_1D, BLOCK_SIZE, Q_1D>(interp_1d, u, temp));
+  CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP, P_1D, BLOCK_SIZE * Q_1D, Q_1D>(grad_1d, temp, &v[BLOCK_SIZE * NUM_COMP * Q_1D * Q_1D]));
   return CEED_ERROR_SUCCESS;
 }
 
@@ -233,9 +233,9 @@ static inline int CeedBasis_Apply_NoTranspose_Grad_Tensor_3D(const CeedScalar *i
   CeedScalar temp_0[BLOCK_SIZE * NUM_COMP * Q_1D * CeedIntMax(Q_1D, P_1D) * CeedIntMax(Q_1D, P_1D)];
   CeedScalar temp_1[BLOCK_SIZE * NUM_COMP * Q_1D * CeedIntMax(Q_1D, P_1D) * CeedIntMax(Q_1D, P_1D)];
 
-  CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP * P_1D * P_1D, P_1D, BLOCK_SIZE, Q_1D>(interp_1d, u, temp_0));
+  CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP * P_1D * P_1D, P_1D, BLOCK_SIZE, Q_1D>(grad_1d, u, temp_0));
   CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP * P_1D, P_1D, BLOCK_SIZE * Q_1D, Q_1D>(interp_1d, temp_0, temp_1));
-  CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP, P_1D, BLOCK_SIZE * Q_1D * Q_1D, Q_1D>(grad_1d, temp_1, v));
+  CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP, P_1D, BLOCK_SIZE * Q_1D * Q_1D, Q_1D>(interp_1d, temp_1, v));
 
   CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP * P_1D * P_1D, P_1D, BLOCK_SIZE, Q_1D>(interp_1d, u, temp_0));
   CeedCall(TensorContract_Apply_NoTranspose<NUM_COMP * P_1D, P_1D, BLOCK_SIZE * Q_1D, Q_1D>(grad_1d, temp_0, temp_1));
