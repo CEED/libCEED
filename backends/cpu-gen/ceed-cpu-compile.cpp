08287285de199a38e4d0905fd7d03a6ea64f0ab3
Jeremy L Thompson <jeremy@jeremylt.org>
2026-09-11 20:28:33 +0200

    wip - debug JiT compile options

--- backends/cpu-gen/ceed-cpu-compile.cpp
+++ backends/cpu-gen/ceed-cpu-compile.cpp
@@ -58,8 +58,11 @@ static inline int CeedJitGetOpts_Cpu(Ceed ceed, const char ***opts, int *num_opt
 
   // Standard options
   CeedCallBackend(CeedCalloc(opts_count, opts));
-  CeedCallBackend(CeedStringAllocCopy("-march=native", (char **)&(*opts)[0]));
-  CeedCallBackend(CeedStringAllocCopy("-O3", (char **)&(*opts)[1]));
+  // TODO: Revert
+  // CeedCallBackend(CeedStringAllocCopy("-march=native", (char **)&(*opts)[0]));
+  // CeedCallBackend(CeedStringAllocCopy("-O3", (char **)&(*opts)[1]));
+  CeedCallBackend(CeedStringAllocCopy("-g1", (char **)&(*opts)[0]));
+  CeedCallBackend(CeedStringAllocCopy("-O0", (char **)&(*opts)[1]));
 
   // Additional include dirs
   {
