16a7d8fa9bfbd82231eaf2f0e637284f87ea3c3b
Jeremy L Thompson <jeremy@jeremylt.org>
2026-09-12 07:43:23 +0200

    cpu(gen) - real pedantic fix

--- backends/cpu-gen/ceed-cpu-compile.h
+++ backends/cpu-gen/ceed-cpu-compile.h
@@ -9,6 +9,17 @@
 #include <ceed.h>
 #include <ceed/backend.h>
 
+#include <dlfcn.h>
+
+#define CeedRunFunction_Cpu(ceed, handle, name, function, ...)                 \
+  do {                                                                         \
+    *(void **)(&op_function) = (void *)dlsym(handle, name);                    \
+    if (function == NULL) {                                                    \
+      return CeedError((ceed), CEED_ERROR_BACKEND, "Failed to load function"); \
+    }                                                                          \
+    CeedCallBackend((*function)(__VA_ARGS__));                                 \
+  } while (0)
+
 CEED_INTERN int CeedCompile_Cpu(Ceed ceed, const char *source, const char *name, void **handle, const CeedInt num_defines, ...);
 CEED_INTERN int CeedTryCompile_Cpu(Ceed ceed, const char *source, const char *name, bool *is_compile_good, void **handle, const CeedInt num_defines,
                                    ...);
