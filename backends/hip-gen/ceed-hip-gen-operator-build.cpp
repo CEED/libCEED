098db8ad6d4151ed784c3e79e681e69beca5f18e
Jeremy L Thompson <jeremy@jeremylt.org>
2026-09-17 21:35:29 +0200

    gpu - use CeedBasisGetChebyshevData

--- backends/hip-gen/ceed-hip-gen-operator-build.cpp
+++ backends/hip-gen/ceed-hip-gen-operator-build.cpp
@@ -256,16 +256,17 @@ static int CeedOperatorBuildKernelFieldData_Hip_gen(std::ostringstream &code, Ce
       if (is_at_points) {
         // AtPoints
         if (!basis_data->d_chebyshev_interp_1d) {
-          CeedSize    interp_bytes;
-          CeedScalar *chebyshev_interp_1d;
+          CeedSize          interp_bytes;
+          const CeedScalar *chebyshev_interp_1d;
+          CeedBasis         basis_chebyshev;
 
+          CeedCallBackend(CeedBasisGetChebyshevData(basis, &basis_chebyshev, NULL));
+          CeedCallBackend(CeedBasisGetInterp1D(basis_chebyshev, &chebyshev_interp_1d));
           interp_bytes = P_1d * Q_1d * sizeof(CeedScalar);
-          CeedCallBackend(CeedCalloc(P_1d * Q_1d, &chebyshev_interp_1d));
-          CeedCallBackend(CeedBasisGetChebyshevInterp1D(basis, chebyshev_interp_1d));
           CeedCallHip(CeedBasisReturnCeed(basis), hipMalloc((void **)&basis_data->d_chebyshev_interp_1d, interp_bytes));
           CeedCallHip(CeedBasisReturnCeed(basis),
                       hipMemcpy(basis_data->d_chebyshev_interp_1d, chebyshev_interp_1d, interp_bytes, hipMemcpyHostToDevice));
-          CeedCallBackend(CeedFree(&chebyshev_interp_1d));
+          CeedCallBackend(CeedBasisDestroy(&basis_chebyshev));
         }
         if (is_input) {
           data->B.inputs[i] = basis_data->d_chebyshev_interp_1d;
@@ -300,16 +301,17 @@ static int CeedOperatorBuildKernelFieldData_Hip_gen(std::ostringstream &code, Ce
       if (is_at_points) {
         // AtPoints
         if (!basis_data->d_chebyshev_interp_1d) {
-          CeedSize    interp_bytes;
-          CeedScalar *chebyshev_interp_1d;
+          CeedSize          interp_bytes;
+          const CeedScalar *chebyshev_interp_1d;
+          CeedBasis         basis_chebyshev;
 
+          CeedCallBackend(CeedBasisGetChebyshevData(basis, &basis_chebyshev, NULL));
+          CeedCallBackend(CeedBasisGetInterp1D(basis_chebyshev, &chebyshev_interp_1d));
           interp_bytes = P_1d * Q_1d * sizeof(CeedScalar);
-          CeedCallBackend(CeedCalloc(P_1d * Q_1d, &chebyshev_interp_1d));
-          CeedCallBackend(CeedBasisGetChebyshevInterp1D(basis, chebyshev_interp_1d));
           CeedCallHip(CeedBasisReturnCeed(basis), hipMalloc((void **)&basis_data->d_chebyshev_interp_1d, interp_bytes));
           CeedCallHip(CeedBasisReturnCeed(basis),
                       hipMemcpy(basis_data->d_chebyshev_interp_1d, chebyshev_interp_1d, interp_bytes, hipMemcpyHostToDevice));
-          CeedCallBackend(CeedFree(&chebyshev_interp_1d));
+          CeedCallBackend(CeedBasisDestroy(&basis_chebyshev));
         }
         if (is_input) {
           data->B.inputs[i] = basis_data->d_chebyshev_interp_1d;
